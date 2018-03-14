5542c71bf64fea91624c66c6823a5b2720ec790b
Ales Teska <ales.teska@teskalabs.com>
2018-03-14 23:31:09 +0100

    Improve the StreamSocket implementation.

--- asab/socket.py
+++ asab/socket.py
@@ -2,6 +2,19 @@ from .abc.service import Service
 
 class StreamSocketServerService(Service):
 
+	'''
+	Example of use:
+
+	class ServiceMyProtocolServer(asab.StreamSocketServerService):
+
+		async def initialize(self, app):
+			host = asab.Config.get('http', 'host')
+			port = asab.Config.getint('http', 'port')
+
+			L.debug("Starting server on {} {} ...".format(host, port))
+			await self.create_server(app, MyProtocol, [(host, port)])
+
+	'''
 
 	def __init__(self, app):
 		super().__init__(app)
@@ -9,9 +22,12 @@ class StreamSocketServerService(Service):
 		app.PubSub.subscribe("Application.exit!", self._on_exit)
 
 
-	async def create_server(self, app, protocol, host, port):
-		server = await app.Loop.create_server(protocol, host, port)
-		self._servers.append(server)
+	async def create_server(self, app, protocol, addrs):
+		#TODO: Paraelize this ...
+		for addr in addrs:
+			host, port = addr
+			server = await app.Loop.create_server(protocol, host, port)
+			self._servers.append(server)
 
 
 	def _on_exit(self, event_name):
