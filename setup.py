0232d381aa60c6461bd757fa81259107c69919de
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2018-03-19 14:30:51 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+	name='asab',
+	version='18.03-beta1',
+	description='Asynchronous Server Application Boilerplate',
+	url='https://github.com/TeskaLabs/asab',
+	author='TeskaLabs Ltd',
+	author_email='support@teskalabs.com',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.6',
+	],
+	keywords='asynchornous server application boilerplate',
+	packages=find_packages(exclude=['doc', 'module_sample']),
+	project_urls={
+		'Source': 'https://github.com/TeskaLabs/asab'
+	},
+)
