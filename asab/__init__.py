388a10b3373b7c29dde2c34586a588da6c5308e4
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2018-03-19 14:28:04 +0100

    Adding __init__.py to abc so that it gets included by setuptools.

--- asab/__init__.py
+++ asab/__init__.py
@@ -2,6 +2,7 @@ from .log import StructuredDataLogger
 from .application import Application
 from .abc.service import Service
 from .abc.module import Module
+from .abc.singleton import Singleton
 from .config import Config
 from .pubsub import subscribe, PubSub
 
