c223bba1b7d976b29bd99e51c46bb77b594e0ae6
Ales Teska <ales.teska@teskalabs.com>
2018-03-20 11:07:45 +0100

    Improve a commentary.

--- asab/abc/singleton.py
+++ asab/abc/singleton.py
@@ -2,14 +2,17 @@
 class Singleton(type):
 
 	"""
-	The singleton pattern is a software design pattern that restricts the instantiation of a class to one object.
-	More at https://en.wikipedia.org/wiki/Singleton_pattern
-	Implementation idea is from https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+The singleton pattern is a software design pattern that restricts the instantiation of a class to one object.
+More at https://en.wikipedia.org/wiki/Singleton_pattern
 
-	Usage:
+Implementation idea is from https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+	
+Usage:
 
-	class MyClass(metaclass=Singleton):
-		...
+.. code:: python
+
+    class MyClass(metaclass=Singleton):
+        ...
 
 	"""
 
