02038c0d8b78a3549d3e90aa2cd2458f587fa5de
Ales Teska <ales.teska@teskalabs.com>
2018-04-03 00:19:02 +0200

    Initial implementation of aiohttp based HTTP server & module web.

---
+++ asab/web/service_webapp.py
@@ -0,0 +1,34 @@
+import logging
+import aiohttp.web
+import asab
+
+#
+
+L = logging.getLogger(__name__)
+
+#
+
+class ServiceWebApp(asab.Service):
+
+
+	def __init__(self, app, service_name):
+		super().__init__(app, service_name)
+
+		# Parse listen address(es), can be multiline configuration item
+		ls = asab.Config["asab:web"]["listen"]
+		self._listen = []
+		for l in ls.split('\n'):
+			addr, port = l.split(' ', 1)
+			port = int(port)
+			self._listen.append((addr, port))
+
+		self.Servers = []
+		self.WebApp = aiohttp.web.Application(loop=app.Loop)
+		self.WebApp['app'] = app
+
+
+	async def initialize(self, app):
+		self.Servers = []
+		for addr, port in self._listen:
+			server = await app.Loop.create_server(self.WebApp.make_handler(), addr, port)
+			self.Servers.append(server)
