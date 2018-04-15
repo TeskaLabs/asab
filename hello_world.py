2761974e773fc824714b21b749e312a7a60793bd
Ales Teska <ales.teska@teskalabs.com>
2018-04-16 00:01:40 +0200

    Improve on the source code readability.

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import asab
 
+
 class MyApplication(asab.Application):
 	async def main(self):
 		print("Hello world")
 		self.stop()
+
 
 if __name__ == '__main__':
 	app = MyApplication()
