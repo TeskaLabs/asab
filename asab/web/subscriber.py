85b37ff035435a1f5ca0f3b7982936c83dab0fa9
Ales Teska <ales.teska@teskalabs.com>
2018-04-21 00:57:19 +0200

    Factoring - split up and down stream.

--- asab/web/subscriber.py
+++ asab/web/subscriber.py
@@ -9,12 +9,25 @@ class Subscriber(object):
 	def __init__(self, app, pubsub, *message_types):
 		self.PubSub = pubsub
 		self.Loop = app.Loop
-		self.MessageTypes = message_types
+
+		for message_type in message_types:
+			self.PubSub.subscribe(message_type, self._on_message)
 
 		#TODO: Applicatio exit:
 		# for ws in self.Websockets:
         #	await ws.close(code=WSCloseCode.GOING_AWAY, message='Server shutdown')
 		self.Websockets = set([])
+
+
+	async def _on_message(self, message_type, *args, **kwargs):
+		message = {'message_type': message_type}
+		if len(args) > 0:
+			message['args'] = args
+		if len(kwargs) > 0:
+			message['kwargs'] = kwargs
+
+		for ws in self.Websockets:
+			await ws.send_json(message)
 
 
 	async def __call__(self, request):
@@ -24,29 +37,10 @@ class Subscriber(object):
 		try:
 			self.Websockets.add(ws)
 
-			subscriber = asab.Subscriber(self.PubSub, *self.MessageTypes)
-			pending = set([subscriber.message(), ws.receive()])
-			while True:
-				done, pending = await asyncio.wait(pending, loop=self.Loop, return_when=asyncio.FIRST_COMPLETED)
-
-				for x in done:
-					x = x.result()
-					if isinstance(x, aiohttp.WSMessage):
-						# We receive a WSMessage from a peer
-						#TODO: Handle this ...
-						pending.add(ws.receive())
-
-					else:
-						# We receive a PubSub message
-						message_type, args, kwargs = x
-						message = {'message_type': message_type}
-						if len(args) > 0:
-							message['args'] = args
-						if len(kwargs) > 0:
-							message['kwargs'] = args
-
-						await ws.send_json(message)
-						pending.add(subscriber.message())
+			async for msg in ws:
+				# We receive a WSMessage from a peer
+				#TODO: Handle this ...
+				print(">>>", msg)
 
 		except asyncio.CancelledError:
 			pass
