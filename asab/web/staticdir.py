f0ef59bc619648de25309b5c237e49a80749d9e3
Ales Teska <ales.teska@teskalabs.com>
2018-08-02 14:02:41 +0200

    Fix of the static dir handler for asab.web

--- asab/web/staticdir.py
+++ asab/web/staticdir.py
@@ -10,20 +10,21 @@ class StaticDirProvider(object):
 	def __init__(self, websvc, root, path, index='index.html'):
 		self.Path = path
 		self.Index = index
+		self.Root = root.rstrip('/')
 
-		websvc.WebApp.router.add_get(root, self.index)
+		websvc.WebApp.router.add_get(self.Root if len(self.Root) > 0 else '/', self._index)
 
 		# Read all files from path (build from React) and map them statically
 		for fname in glob.glob(os.path.join(self.Path, '*')):
 			if os.path.isfile(fname):
-				websvc.WebApp.router.add_get(root+os.path.basename(fname), functools.partial(self.file, fname))
+				websvc.WebApp.router.add_get(self.Root + '/' + os.path.basename(fname), functools.partial(self._file, fname))
 			elif os.path.isdir(fname):
-				websvc.WebApp.router.add_static(root+os.path.basename(fname), path=fname)
+				websvc.WebApp.router.add_static(self.Root + '/' + os.path.basename(fname), path=fname)
 
 
-	async def index(self, request):
+	async def _index(self, request):
 		return aiohttp.web.FileResponse(os.path.join(self.Path, self.Index))
 
 
-	async def file(self, fname, request):
+	async def _file(self, fname, request):
 		return aiohttp.web.FileResponse(fname)
