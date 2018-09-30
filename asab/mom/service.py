066ffc6a5e4e91ef27bd6d3934d8f9564bc8d98d
Ales Teska <ales.teska@teskalabs.com>
2018-09-30 13:30:52 +0200

    Removal of the obsolete attribute.

--- asab/mom/service.py
+++ asab/mom/service.py
@@ -6,7 +6,6 @@ class MOMService(asab.Service):
 	def __init__(self, app, service_name):
 		super().__init__(app, service_name)
 		self.Brokers = set()
-		self.Loop = app.Loop
 
 
 	def _register_broker(self, broker):
