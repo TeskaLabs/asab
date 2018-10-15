8191ff4e7ffadfca17e492f4ef69be99c4fc433f
Premysl Cerny <33154934+PremyslCerny@users.noreply.github.com>
2018-10-15 14:55:44 +0200

    Adding authentication option to Metrics InfluxDB. (#31)

--- asab/metrics/influxdb.py
+++ asab/metrics/influxdb.py
@@ -1,5 +1,6 @@
 import logging
 import aiohttp
+import urllib
 
 import asab
 
@@ -43,6 +44,8 @@ class MetricsInfluxDB(asab.ConfigObject):
 	ConfigDefaults = {
 		'url': 'http://localhost:8086/',
 		'db': 'mydb',
+		'username': '',
+		'password': '',
 	}
 
 
@@ -51,6 +54,13 @@ class MetricsInfluxDB(asab.ConfigObject):
 
 		self.WriteURL = '{}/write?db={}'.format(self.Config.get('url').rstrip('/'), self.Config.get('db'))
 
+		username = self.Config.get('username')
+		if username is not None and len(username) > 0:
+			self.WriteURL += '&u={}'.format(urllib.parse.quote(username, safe=''))
+
+		password = self.Config.get('password')
+		if password is not None and len(password) > 0:
+			self.WriteURL += '&p={}'.format(urllib.parse.quote(password, safe=''))
 
 	async def process(self, now, mlist):
 
@@ -61,4 +71,3 @@ class MetricsInfluxDB(asab.ConfigObject):
 				response = await resp.text()
 				if resp.status != 204:
 					L.warning("Error when sending metrics to Influx: {}\n{}".format(resp.status, response))
-
