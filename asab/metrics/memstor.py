60a15752e9e22dc512d364e285b9dbb671f5d55b
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2019-01-14 13:24:56 +0100

    Adding rest_get to metrics' memory storage target and storing tags too.

--- asab/metrics/memstor.py
+++ asab/metrics/memstor.py
@@ -4,7 +4,23 @@ class MetricsMemstorTarget(asab.ConfigObject):
 
 	def __init__(self, svc, config_section_name, config=None):
 		self.Values = {}
+		self.Tags = {}
 
 	async def process(self, now, mlist):
 		for metric, values in mlist:
+			self.Tags[metric.Name] = metric.Tags
 			self.Values[metric.Name] = values
+
+	def rest_get(self):
+		ret = {}
+		for name, values in self.Values.items():
+			ret[name] = {}
+			ret[name]["Name"] = name
+			ret[name]["Values"] = values
+		for name, tags in self.Tags.items():
+			if ret.get(name) is None:
+				ret[name] = {}
+				ret[name]["Name"] = name
+			ret[name]["Tags"] = tags
+
+		return ret
