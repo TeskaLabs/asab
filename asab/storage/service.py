2a3a46e931bdbfd5fae0cd9c4154f0f9b1723969
Jakub Boukal <www.bagr@gmail.com>
2019-06-10 15:41:49 +0200

    Extend documentation on StorageServiceABC.upsertor

--- asab/storage/service.py
+++ asab/storage/service.py
@@ -1,6 +1,7 @@
 import abc
 import asab
 
+
 class StorageServiceABC(asab.Service):
 	
 	def __init__(self, app, service_name):
@@ -8,17 +9,21 @@ class StorageServiceABC(asab.Service):
 
 
 	@abc.abstractmethod
-	def upsertor(self, collection:str, obj_id=None, version=0):
+	def upsertor(self, collection: str, obj_id=None, version: int = 0):
 		'''
+		:param str collection: Name of collection to work with
+		:param obj_id: Identification of object. \
+						If None and version = 0, the `obj_id` will be auto-generated \
+						If specified, update operation is expected.
 		:param int version: Specify a current version of the object and hence prevent race conditions on updates. \
-							If None, the check is skipped. \
-							If 0, the insert operation is expected.
+							If 0, the insert operation is expected. \
+							Otherwise update operation is expected. `obj_id` must be set
 		'''
 		pass
 
 
 	@abc.abstractmethod
-	async def get(self, collection:str, obj_id) -> dict:
+	async def get(self, collection: str, obj_id) -> dict:
 		"""
 		Get object from collection
 
@@ -33,10 +38,10 @@ class StorageServiceABC(asab.Service):
 
 
 	@abc.abstractmethod
-	async def get_by(self, collection:str, key:str, value):
+	async def get_by(self, collection: str, key: str, value):
 		pass
 
 
 	@abc.abstractmethod
-	async def delete(self, collection:str, obj_id):
+	async def delete(self, collection: str, obj_id):
 		pass
