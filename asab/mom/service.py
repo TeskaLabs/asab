c5abcd6c9903c2c3f3febe3f1691ab5d6786cbd1
Premysl Cerny <33154934+PremyslCerny@users.noreply.github.com>
2019-09-19 15:51:20 +0200

    Improve documentation of ASAB MOM module. (#63)

--- asab/mom/service.py
+++ asab/mom/service.py
@@ -2,6 +2,14 @@ import asab
 
 
 class MOMService(asab.Service):
+	"""
+	MOMService implements message-oriented middleware:
+	https://en.wikipedia.org/wiki/Message-oriented_middleware
+
+	MOMService register brokers to post tasks to the task queue
+	or process tasks and post them to the reply queue.
+	"""
+
 
 	def __init__(self, app, service_name):
 		super().__init__(app, service_name)
