613b62d613de6b09de732f596f31b625a190d91c
Premysl Cerny <33154934+PremyslCerny@users.noreply.github.com>
2019-10-07 10:18:22 +0200

    Work on simple cache to cache identities in dictionary. (#67)

--- asab/web/authn/oauth/middleware.py
+++ asab/web/authn/oauth/middleware.py
@@ -1,5 +1,6 @@
 import aiohttp
 import logging
+import time
 
 #
 
@@ -8,10 +9,13 @@ L = logging.getLogger(__name__)
 #
 
 
-def oauthclient_middleware_factory(app, *args, methods, **kwargs):
+def oauthclient_middleware_factory(app, *args, methods, identity_cache_longevity=60*60, **kwargs):
 	"""
 	Serves to connect with the user info endpoint of OAuth 2.0 server to obtain identity of the user
 	associated with the provided access token.
+
+	:methods is a list that specifies the identification of OAuth servers such as [asab.web.authn.oauth.GitHubOAuthMethod()]
+	:identity_cache_longevity is an integer that specifies the number of seconds after which the cached identity expires
 
 	The expected format of Authorization header is:
 	Authorization: Bearer <OAUTH-SERVER-ID>-<ACCESS_TOKEN>
@@ -19,6 +23,8 @@ def oauthclient_middleware_factory(app, *args, methods, **kwargs):
 	For more information about user info, visit:
 	https://connect2id.com/products/server/docs/api/userinfo
 	"""
+
+	identity_cache = {}
 
 	methods_dict = {}
 	for method in methods:
@@ -37,12 +43,24 @@ def oauthclient_middleware_factory(app, *args, methods, **kwargs):
 			return await handler(request)
 
 		bearer = bearer_oauth[0]
+		oauth_server_id_access_token = bearer_oauth[1]
 
-		if "-" not in bearer_oauth[1]:
+		if "-" not in oauth_server_id_access_token:
 			L.warn("Authorization header's bearer '{}' is not in proper '<OAUTH-SERVER-ID>-<ACCESS_TOKEN>' format.".format(bearer_oauth[1]))
 			return await handler(request)
 
-		oauth_server_id, access_token = bearer_oauth[1].split('-', 1)
+		identity = identity_cache.get(oauth_server_id_access_token)
+		if identity is not None:
+			# This is "cache hit" branch
+			expiration = identity.get("Expiration", time.time())
+			if expiration < time.time():
+				del identity_cache[oauth_server_id_access_token]
+			else:
+				request.OAuthUserInfo = identity.get("OAuthUserInfo")
+				request.Identity = identity.get("Identity")
+				return await handler(request)
+
+		oauth_server_id, access_token = oauth_server_id_access_token.split('-', 1)
 
 		method = methods_dict.get(oauth_server_id)
 
@@ -58,6 +76,11 @@ def oauthclient_middleware_factory(app, *args, methods, **kwargs):
 					if oauth_user_info is not None:
 						request.OAuthUserInfo = oauth_user_info
 						request.Identity = method.get_identity_from_oauth_user_info(oauth_user_info)
+						identity_cache[oauth_server_id_access_token] = {
+							"OAuthUserInfo": request.OAuthUserInfo,
+							"Identity": request.Identity,
+							"Expiration": time.time() + identity_cache_longevity
+						}
 				else:
 					raise RuntimeError("Call to OAuth server '{}' failed with status code '{}'.".format(oauth_userinfo_url, resp.status))
 
