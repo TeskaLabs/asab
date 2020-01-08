98b03e7ca851b064293ff86ea4836833a8e8617d
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2020-01-08 18:31:50 +0100

    Making RootLogger's logging level configurable. (#90)
    * Making RootLogger's logging level configurable.
    
    * Fixing typo,.

--- asab/config.py
+++ asab/config.py
@@ -40,6 +40,7 @@ class ConfigParser(configparser.ConfigParser):
 			'verbose': os.environ.get('ASAB_VERBOSE', False),
 			"app_name": os.path.basename(sys.argv[0]),
 			"sd_id": "sd",  # Structured data id, see RFC5424
+			"level": "NOTICE",
 		},
 
 		"logging:console": {
