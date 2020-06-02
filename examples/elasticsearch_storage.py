b57eaf470ea45c64f5680c1809461e13e4d1dc0e
Ales Teska <ales.teska@teskalabs.com>
2020-06-02 14:14:11 +0200

    Review completed.

---
+++ examples/elasticsearch_storage.py
@@ -0,0 +1,63 @@
+import pprint
+
+import asab
+import asab.storage
+
+
+asab.Config.add_defaults(
+	{
+		'asab:storage': {
+			'type': 'elasticsearch',
+			'elasticsearch_url': 'http://10.17.174.124:9200/',
+		}
+	}
+)
+
+
+class MyApplication(asab.Application):
+
+	async def initialize(self):
+
+		# Loading the web service module
+		self.add_module(asab.storage.Module)
+
+
+	async def main(self):
+		storage = self.get_service("asab.StorageService")
+
+		# Obtain upsertor object which is associated with given "test-collection"
+		# To create new object we keep default `version` to zero
+		u = storage.upsertor("test-collection")
+		u.set("foo", "bar")
+		objid = await u.execute()
+
+		obj = await storage.get("test-collection", objid)
+		# Obtain upsertor object for update - specify existing `version` number
+		u = storage.upsertor("test-collection", obj_id=objid, version=obj['_v'])
+		u.set("foo", "buzz")
+		objid = await u.execute()
+
+		obj = await storage.get("test-collection", objid)
+		print("Result of get by id '{}'".format(objid))
+		pprint.pprint(obj)
+
+		await storage.delete("test-collection", objid)
+
+
+		# Insert the document with provided ObjId
+		u = storage.upsertor("test-collection", "test")
+		u.set("foo", "bar")
+		objid = await u.execute()
+
+		obj = await storage.get("test-collection", objid)
+		print("Result of get by id '{}'".format(objid))
+		pprint.pprint(obj)
+
+		await storage.delete("test-collection", objid)
+
+		self.stop()
+
+
+if __name__ == '__main__':
+	app = MyApplication()
+	app.run()
