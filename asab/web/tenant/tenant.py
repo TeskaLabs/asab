bea6775bef5f8e3f37aa08ffad20f00b78d4ddae
Ales Teska <ales.teska@teskalabs.com>
2020-09-23 16:19:05 +0200

    Unifying tenant calls to be same as in the SeaCat Auth

--- asab/web/tenant/tenant.py
+++ asab/web/tenant/tenant.py
@@ -3,13 +3,12 @@
 class Tenant(object):
 
 	def __init__(self, tenant_id: str, params: dict = {}):
-
 		self.Id = tenant_id
 		self.Parameters = params
 
-	def to_dict(self):
 
+	def to_dict(self):
 		return {
-			'id': self.Id,
+			'_id': self.Id,
 			'parameters': self.Parameters
 		}
