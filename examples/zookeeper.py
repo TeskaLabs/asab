3399cad59bf6e1fc8d5dd8c1543eb5fa86cb170f
mithunbharadwaj <mithunshivashankar@gmail.com>
2020-12-30 09:41:29 +0100

    write list of files

--- examples/zookeeper.py
+++ examples/zookeeper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
 import sys
+import asab.zookeeper
+import configparser
 import asab.config
 from asab.config import Config
 
@@ -19,14 +20,17 @@ class MyApplication(asab.Application):
 	def __init__(self):
 		super().__init__()
 
-	# Get the list with contents of the configuration files
-		cont_list = Config.get_config_contents_list()
-		for list in cont_list:
-			print(list)
+	 #Get the list with contents of the configuration files
+		config = configparser.ConfigParser()
+		cont_list ,cont_name = Config.get_config_contents_list()
+		for list,name in zip(cont_list,cont_name):
+			config.read_string(list)
+			with open(name, 'w') as wconfigfile:
+				config.write(wconfigfile)
 		sys.exit(1)
+
 
 if __name__ == "__main__":
 	app = MyApplication()
 	app.run()
-
 
