1bb7f6c67b18b1002bd5aa7c8138cd3c5649a00f
Ales Teska <ales.teska@teskalabs.com>
2021-02-10 00:54:15 +0100

    Extends the information provided to a ZooKeeper.

--- asab/api/service.py
+++ asab/api/service.py
@@ -19,10 +19,10 @@ class ApiService(asab.Service):
 		super().__init__(app, service_name)
 
 		listen = asab.Config["asab:web"]["listen"]
-		self.WebContainer = self._initialize_web(app, listen)
+		self.WebContainer = self._initialize_web(listen)
 
 		if len(asab.Config["asab:zookeeper"]["servers"]) > 0:
-			self.ZkContainer = self._initialize_zookeeper(app)
+			self.ZkContainer = self._initialize_zookeeper()
 		else:
 			self.ZkContainer = None
 
@@ -40,22 +40,23 @@ class ApiService(asab.Service):
 		if self.ZkContainer is not None:
 			await self.ZkContainer.ZooKeeper.ensure_path(self.ZkContainer.ZooKeeperPath + '/run')
 			await self.ZkContainer.advertise(
-				data=self._build_zookeeper_adv_data(self.App),
-				path="run/a.",
+				data=self._build_zookeeper_adv_data(),
+				path="run/{}.".format(self.App.__class__.__name__),
 			)
 
 
-	def _build_zookeeper_adv_data(self, app):
+	def _build_zookeeper_adv_data(self):
 		adv_data = {
-			'hostname': app.HostName,
+			'appclass': self.App.__class__.__name__,
+			'hostname': self.App.HostName,
 		}
 		if self.WebContainer is not None:
 			adv_data['web'] = self.WebContainer.Addresses
 		return adv_data
 
 
-	def _initialize_web(self, app, listen):
-		websvc = app.get_service("asab.WebService")
+	def _initialize_web(self, listen):
+		websvc = self.App.get_service("asab.WebService")
 
 		# Create a dedicated web container
 		container = asab.web.WebContainer(
@@ -65,7 +66,7 @@ class ApiService(asab.Service):
 		# TODO: refactor to use custom config section, instead of explicitly passing "listen" param?
 
 		# TODO: Logging level configurable via config file
-		self.APILogHandler = WebApiLoggingHandler(app, level=logging.NOTSET)
+		self.APILogHandler = WebApiLoggingHandler(self.App, level=logging.NOTSET)
 		self.format = logging.Formatter("%%(asctime)s %%(levelname)s %%(name)s %%(struct_data)s%%(message)s")
 		self.APILogHandler.setFormatter(self.format)
 		self.Logging = logging.getLogger()
@@ -99,13 +100,13 @@ class ApiService(asab.Service):
 		return asab.web.rest.json_response(request, result)
 
 
-	def _initialize_zookeeper(self, app):
+	def _initialize_zookeeper(self):
 		from ..zookeeper import Module as zkModule, ZooKeeperContainer
-		app.add_module(zkModule)
+		self.App.add_module(zkModule)
 
-		container = ZooKeeperContainer(app, "asab:zookeeper")
+		container = ZooKeeperContainer(self.App, "asab:zookeeper")
 
-		zksvc = app.get_service("asab.ZooKeeperService")
+		zksvc = self.App.get_service("asab.ZooKeeperService")
 		zksvc.register_container(container)
 
 		return container
