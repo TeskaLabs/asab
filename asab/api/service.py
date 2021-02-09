2a837c8ac5fbee4431c948d5b0f03eba1ae84968
Jakub Schier <jakub.schier@gmail.com>
2021-02-09 14:23:39 +0100

    update check for zookeeper section

--- asab/api/service.py
+++ asab/api/service.py
@@ -21,7 +21,7 @@ class ApiService(asab.Service):
 		listen = asab.Config["asab:web"]["listen"]
 		self.WebContainer = self._initialize_web(app, listen)
 
-		if len(asab.Config["asab:zookeeper"]["servers"]) > 0:
+		if asab.Config.has_section("asab:zookeeper"]):
 			self.ZkContainer = self._initialize_zookeeper(app)
 		else:
 			self.ZkContainer = None
