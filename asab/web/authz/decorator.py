53305d325a29c0b7edbf139f943a92769621b1b9
Přemysl Černý <premysl.cerny@teskalabs.com>
2021-02-18 12:15:11 +0100

    Add a simple cache for required decorator with TODO.

--- asab/web/authz/decorator.py
+++ asab/web/authz/decorator.py
@@ -1,9 +1,12 @@
 import functools
+import time
 
 import aiohttp.web
 import aiohttp.hdrs
 
 from ...config import Config
+
+required_cache_dict = dict()
 
 
 def required(*resources):
@@ -11,6 +14,8 @@ def required(*resources):
 	Checks that user authorized with access token in
 	Authorization header has access to a given tenant space
 	using SeaCat Auth RBAC authorization.
+
+	It uses a cache to limit the number of HTTP checks.
 
 	Example of use:
 
@@ -33,22 +38,53 @@ def required(*resources):
 			tenant = request.Tenant
 			rbac_url = Config["authz"]["rbac_url"]
 
+			current_time = time.time()
+
+			# TODO: Replace cache invalidation with more clever approach like session indicator in the HTTP request
+			cache_keys_to_delete = list()
+
+			for cache_key, cache_value in required_cache_dict.items():
+				authorized, expiration = cache_value
+				if current_time >= expiration:
+					cache_keys_to_delete.append(cache_key)
+
+			for cache_key in cache_keys_to_delete:
+				del required_cache_dict[cache_key]
+
 			# Check authorization using RBAC
 			# Authorization header should already be part of the request
 			for resource in resources:
+
+				# Check that the item is located in the cache
+				tenant_id = tenant.Id if hasattr(tenant, "Id") else tenant["_id"]
+				cache_key = "{};{}".format(tenant_id, resource)
+				cache_value = required_cache_dict.get(cache_key)
+
+				if cache_value is not None:
+					authorized, expiration = cache_value
+
+					if not authorized:
+						raise aiohttp.web.HTTPUnauthorized()
+					else:
+						continue
+
 				async with aiohttp.ClientSession() as session:
 
 					async with session.get(
 							"{}/{}/{}".format(
 								rbac_url,
-								tenant.Id if hasattr(tenant, "Id") else tenant["_id"],
+								tenant_id,
 								resource,
 							),
 							headers={
 								"Authorization": request.headers.get(aiohttp.hdrs.AUTHORIZATION, None)
 							}
 					) as resp:
-						if resp.status != 200:
+
+						authorized = resp.status == 200
+						required_cache_dict[cache_key] = (authorized, current_time + 300)
+
+						if not authorized:
 							raise aiohttp.web.HTTPUnauthorized()
 
 		return wrapper
