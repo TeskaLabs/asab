ef8409c8cb4739867efbe6b479a8b51eaff9b672
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-02-20 16:32:28 +0100

    zookeeper builder module - update doc

--- asab/zookeeper/builder.py
+++ asab/zookeeper/builder.py
@@ -3,6 +3,28 @@ import aiozk
 
 """
 This module builds ZooKeeper clients from Configs and urls
+urls supported :
+ 1. Absolute url 
+	Example: zookeeper://zookeeper:12181/etc/configs/file1
+	
+ 2. Relative ur1 with full path
+	Example: zookeeper:///etc/configs/file1
+	In this case the relative url is expanded as follows:
+	zookeeper://{default_server}/etc/configs/file1
+	Where {default_server} is substituted with the server entry of the [asab:zookeeper] configuration file section.
+
+ 3. Relative url with relative path
+	Example: zookeeper:./etc/configs/file1
+	In this case, the relative url is expanded as follows:
+	zookeper://{default_server}/{default_path}/etc/configs/file1
+	Where {default_server} is substituted with the "server" entry of the [asab:zookeeper] configuration file section and
+	{default_path} is substituted with the "path" entry of the [asab:zookeeper] configuration file section.
+
+Sample config file:
+
+[asab.zookeeper]
+server=server1 server2 server3      <-- Default server
+path=/myfolder                      <-- Default path
 """
 
 async def build_client(Config, z_url):
@@ -12,7 +34,7 @@ async def build_client(Config, z_url):
 
 	#If there is no location, use implied
 	if not url_netloc:
-		url_netloc = Config["asab:zookeper"]["servers"]
+		url_netloc = Config["asab:zookeeper"]["servers"]
 
 	#Create and return the client
 	client = aiozk.ZKClient(url_netloc)
