2af375c787237402e889bb5cb7140e85445711e0
Přemysl Černý <premysl.cerny@teskalabs.com>
2021-04-14 14:56:38 +0200

    Authz: Fix decorator. Enable DISABLE of authorization checks.

--- asab/web/authz/__init__.py
+++ asab/web/authz/__init__.py
@@ -1,12 +1,5 @@
 from ...config import Config
 
-Config.add_defaults({
-	"authz": {
-		"rbac_url": "http://localhost:8081/rbac",
-	},
-})
-
-
 from .decorator import required
 from .middleware import authz_middleware_factory
 from .service import AuthzService
