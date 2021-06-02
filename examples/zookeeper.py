e37028f68344d62e57a9f7724e89fcfedc05ba30
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-06-02 08:15:41 +0200

    Zookeeper-builder : Small corrections in return

--- examples/zookeeper.py
+++ examples/zookeeper.py
@@ -2,7 +2,7 @@
 import sys
 import asab.config
 from asab.config import Config
-
+from asab.zookeeper.builder import build_client
 
 L = asab.config.logging.getLogger(__name__)
 
@@ -17,7 +17,12 @@ class MyApplication(asab.Application):
 
 	def __init__(self):
 		super().__init__()
+		if not asab.Config.has_option('asab:zookeeper', 'providers'):
+			z_path = None
+		else:
+			z_path = self.ZooKeeperPath = asab.Config.get('asab:zookeeper', 'providers')
 		# Get the list with contents of the configuration files and its name respectively
+		path , clint = asab.zookeeper.builder.build_client(asab.Config ,z_path)
 		cont_list, cont_name = Config.get_config_contents_list()
 		for list, name in zip(cont_list, cont_name):
 			print("Printing config file: " + name)
