b9326fb286d3acea11cbe2729abc1948664dcee9
rhruska <robin.hruska@teskalabs.com>
2021-06-16 15:14:25 +0200

    change "asab:web" to "web" in app

--- asab/application.py
+++ asab/application.py
@@ -128,7 +128,10 @@ class Application(metaclass=Singleton):
 		self.TaskService = TaskService(self)
 
 		# Setup ASAB API
-		if len(Config['asab:web']["listen"]) > 0:
+		web_config = Config.get("web")
+		if web_config is None:
+			web_config = Config.get("asab:web")
+		if len(web_config["listen"]) > 0:
 			from asab.api import Module
 			self.add_module(Module)
 
@@ -195,7 +198,7 @@ class Application(metaclass=Singleton):
 			Config._default_values['logging:file']['path'] = args.log_file
 
 		if args.web_api:
-				Config._default_values['asab:web']['listen'] = args.web_api
+			Config._default_values['web']['listen'] = args.web_api
 		return args
 
 
