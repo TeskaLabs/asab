d0b3b257ca8af059e098388bed1a9094dbc3fab1
Ales Teska <ales.teska@teskalabs.com>
2021-07-13 17:43:02 +0200

    Refactoring API service.

--- asab/api/__init__.py
+++ asab/api/__init__.py
@@ -1,14 +1,5 @@
-import asab
-import asab.web
-
 from .service import ApiService
 
-
-class Module(asab.Module):
-
-	def __init__(self, app):
-		super().__init__(app)
-		Module = asab.web.Module
-		app.add_module(Module)
-
-		self.Service = ApiService(app, "asab.ApiService")
+__all__ = [
+	"ApiService"
+]
--- asab/api/service.py
+++ asab/api/service.py
@@ -1,4 +1,3 @@
-import os
 import datetime
 import logging
 
@@ -6,8 +5,9 @@ import logging
 import asab
 import asab.web
 import asab.web.rest
-import aiohttp
-from asab.api.log import WebApiLoggingHandler
+
+from web_handler import APIWebHandler
+from .log import WebApiLoggingHandler
 
 ##
 
@@ -19,28 +19,44 @@ L = logging.getLogger(__name__)
 
 class ApiService(asab.Service):
 
+
 	def __init__(self, app, service_name):
 		super().__init__(app, service_name)
-
-		self.WebApp = self._initialize_web()
-
-		if len(asab.Config["asab:zookeeper"]["servers"]) > 0:
-			self.ZkContainer = self._initialize_zookeeper()
-		else:
-			self.ZkContainer = None
+		self.WebHandler = None
+		self.ZkContainer = None
 
 
-	async def _on_webcontainer_started(self, event_name, container):
+	def initialize_web(self, webapp):
+		'''
+		Example:
 
-		if self.WebContainer != container:
-			return
+		websvc = self.App.get_service("asab.WebService")
+		ApiService.initialize_web(websvc.WebApp)
+		'''
+		assert self.WebHandler is None
 
-		if self.ZkContainer is not None:
-			await self.ZkContainer.ZooKeeper.ensure_path(self.ZkContainer.ZooKeeperPath + '/run')
-			await self.ZkContainer.advertise(
-				data=self._build_zookeeper_adv_data(),
-				path="run/{}.".format(self.App.__class__.__name__),
-			)
+		# TODO: Logging level configurable via config file
+		self.APILogHandler = WebApiLoggingHandler(self.App, level=logging.NOTSET)
+		self.format = logging.Formatter("%%(asctime)s %%(levelname)s %%(name)s %%(struct_data)s%%(message)s")
+		self.APILogHandler.setFormatter(self.format)
+		self.Logging = logging.getLogger()
+		self.Logging.addHandler(self.APILogHandler)
+
+		self.WebHandler = APIWebHandler(self.App, webapp)
+
+
+	def initialize_zookeeper(self, zksvc):
+		'''
+		Example:
+
+		zksvc = self.App.get_service("asab.ZooKeeperService")
+		ApiService.initialize_zookeeper(zksvc)
+		'''
+		assert self.ZkContainer is None
+
+		# get zookeeper-serivice
+		self.ZkContainer = zksvc.build_container()
+		# TODO: There is something likely missing
 
 
 	def _build_zookeeper_adv_data(self):
@@ -52,75 +68,3 @@ class ApiService(asab.Service):
 		if self.WebContainer is not None:
 			adv_data['web'] = self.WebContainer.Addresses
 		return adv_data
-
-
-	def _initialize_web(self):
-		websvc = self.App.get_service("asab.WebService")
-
-		# TODO: Logging level configurable via config file
-		self.APILogHandler = WebApiLoggingHandler(self.App, level=logging.NOTSET)
-		self.format = logging.Formatter("%%(asctime)s %%(levelname)s %%(name)s %%(struct_data)s%%(message)s")
-		self.APILogHandler.setFormatter(self.format)
-		self.Logging = logging.getLogger()
-		self.Logging.addHandler(self.APILogHandler)
-
-		# Add routes
-		websvc.WebApp.router.add_get('/asab/v1/environ', self.environ)
-		websvc.WebApp.router.add_get('/asab/v1/config', self.config)
-
-		websvc.WebApp.router.add_get('/asab/v1/logs', self.APILogHandler.get_logs)
-		websvc.WebApp.router.add_get('/asab/v1/logws', self.APILogHandler.ws)
-
-		websvc.WebApp.router.add_get('/asab/v1/changelog', self.changelog)
-
-		return websvc.WebApp
-
-
-	async def environ(self, request):
-		return asab.web.rest.json_response(request, dict(os.environ))
-
-
-	async def config(self, request):
-		# Copy the config and erase all passwords
-		result = {}
-		for section in asab.Config.sections():
-			result[section] = {}
-			# Access items in the raw mode (they are not interpolated)
-			for option, value in asab.Config.items(section, raw=True):
-				if section == "passwords":
-					result[section][option] = "***"
-				else:
-					result[section][option] = value
-		return asab.web.rest.json_response(request, result)
-
-
-	def _initialize_zookeeper(self):
-
-		from ..zookeeper import Module as zkModule
-		self.App.add_module(zkModule)
-
-		# get zookeeper-serivice
-		zksvc = self.App.get_service("asab.ZooKeeperService")
-		return zksvc.build_container()
-
-
-	def changelog_path(self):
-		path = asab.Config.get('general', 'changelog_path')
-		if os.path.isfile(path):
-			return path
-		if os.path.isfile('/CHANGELOG.md'):
-			return '/CHANGELOG.md'
-		if os.path.isfile('CHANGELOG.md'):
-			return 'CHANGELOG.md'
-		return None
-
-
-	def changelog(self, request):
-		path = self.changelog_path()
-
-		if path is None:
-			return aiohttp.web.HTTPNotFound()
-
-		with open(path) as f:
-			result = f.read()
-		return aiohttp.web.Response(text=result, content_type='text/markdown')
