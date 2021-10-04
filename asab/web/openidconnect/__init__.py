ddd0cae458dd5b49cd1764240ae59f58875e96ea
rhruska <robin.hruska@teskalabs.com>
2021-10-04 17:48:25 +0200

    OpenIDConnectService: delete decorator for now

--- asab/web/openidconnect/__init__.py
+++ asab/web/openidconnect/__init__.py
@@ -5,9 +5,8 @@ from .service import OpenIDConnectService
 
 class Module(asab.Module):
 	"""
-	OAuth Client Module provides OAuthClientService and oauthclient_middleware_factory
-	to connect with the user info endpoint of OAuth 2.0 server to obtain identity of the user
-	associated with the provided access token.
+	OpenIDConnect Module provides OpenIDConnectService which supplies the userinfo data provided by
+	the authorization server.
 	"""
 
 	def __init__(self, app):
