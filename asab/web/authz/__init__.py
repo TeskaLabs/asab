b4ece2a30263b4ba0121cdf302166c39398e563f
rhruska <robin.hruska@teskalabs.com>
2021-10-06 17:59:17 +0200

    Authz decorators: Create userinfo decorator

--- asab/web/authz/__init__.py
+++ asab/web/authz/__init__.py
@@ -1,11 +1,10 @@
-from ...config import Config
-
-from .decorator import required
+from .decorator import required, userinfo
 from .middleware import authz_middleware_factory
 from .service import AuthzService
 
 __all__ = (
 	"required",
+	"userinfo",
 	"authz_middleware_factory",
 	"AuthzService"
 )
--- asab/web/authz/decorator.py
+++ asab/web/authz/decorator.py
@@ -22,8 +22,6 @@ def required(*resources):
 		...
 	'''
 
-	authorization_header_rg = re.compile(r"^\s*Bearer ([A-Za-z0-9\-\.\+_~/=]*)")
-
 	def decorator_required(func):
 
 		@functools.wraps(func)
@@ -38,14 +36,7 @@ def required(*resources):
 			# Obtain authz service from the request
 			authz_service = request.AuthzService
 
-			access_token = None
-			authorization_header = request.headers.get(aiohttp.hdrs.AUTHORIZATION, None)
-
-			# Obtain access token from the authorization header
-			if authorization_header is not None:
-				authorization_match = authorization_header_rg.match(authorization_header)
-				if authorization_match is not None:
-					access_token = authorization_match.group(1)
+			access_token = _get_access_token(request)
 
 			# For resistancy against security attacks
 			if access_token is None:
@@ -64,3 +55,63 @@ def required(*resources):
 		return wrapper
 
 	return decorator_required
+
+
+def userinfo(func):
+	"""
+	Fetches userinfo and passes the response dict to the decorated function.
+
+	It uses a cache to limit the number of HTTP checks.
+
+	Example of use:
+
+	@asab.web.tenant.tenant_handler
+	@asab.web.authz.userinfo
+	async def endpoint(self, request, *, tenant, userinfo):
+		...
+	"""
+
+	@functools.wraps(func)
+	async def wrapper(*args, **kargs):
+
+		# RBAC URL is disabled, so no authorization can be performed
+		if Config["authz"]["rbac_url"] == "!DISABLED!":
+			return await func(*args, **kargs)
+
+		request = args[-1]
+
+		# Obtain authz service from the request
+		authz_service = request.AuthzService
+
+		access_token = _get_access_token(request)
+
+		# Fail if no access token is found in the request
+		if access_token is None:
+			raise aiohttp.web.HTTPUnauthorized()
+
+		userinfo_data = await authz_service.userinfo(
+			access_token=access_token,
+			tenant=getattr(request, "Tenant", None),
+		)
+		if userinfo_data is not None:
+			return await func(*args, userinfo=userinfo_data, **kargs)
+
+		# Be defensive
+		raise aiohttp.web.HTTPUnauthorized()
+
+	return wrapper
+
+
+def _get_access_token(request):
+	authorization_header_rg = re.compile(r"^\s*Bearer ([A-Za-z0-9\-\.\+_~/=]*)")
+
+	authorization_header = request.headers.get(aiohttp.hdrs.AUTHORIZATION, None)
+	access_token = None
+
+	# Obtain access token from the authorization header
+	if authorization_header is not None:
+		authorization_match = authorization_header_rg.match(authorization_header)
+		if authorization_match is not None:
+			access_token = authorization_match.group(1)
+
+	return access_token
