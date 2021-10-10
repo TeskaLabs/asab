7c2ec03dff51ccc74f68898835eec6be44a3cac0
Robin Hruška <11579460+byewokko@users.noreply.github.com>
2021-10-10 17:21:58 +0200

    Userinfo decorator (#173)
    * AuthzService: Add userinfo method, refactor authorize method to use userinfo
    
    * Authz decorators: Create userinfo decorator
    
    * Authz decorators: Add usage example
    
    * Authz userinfo: Name refactoring
    
    * Authz decorator: Fix URL check
    
    * Authz service: Fix userinfo URL
    
    Co-authored-by: rhruska <robin.hruska@teskalabs.com>

--- asab/web/authz/__init__.py
+++ asab/web/authz/__init__.py
@@ -1,11 +1,10 @@
-from ...config import Config
-
-from .decorator import required
+from .decorator import required, userinfo_handler
 from .middleware import authz_middleware_factory
 from .service import AuthzService
 
 __all__ = (
 	"required",
+	"userinfo_handler",
 	"authz_middleware_factory",
 	"AuthzService"
 )
