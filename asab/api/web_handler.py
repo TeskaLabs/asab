a43ead62e2391426d001532261ba2ccf1aff2231
eliska <eliska.novotna@protonmail.com>
2022-01-14 13:58:00 +0100

    counter in OM format accepted by Prometheus

--- asab/api/web_handler.py
+++ asab/api/web_handler.py
@@ -6,6 +6,8 @@ import aiohttp.web
 class APIWebHandler(object):
 
 	def __init__(self, app, webapp, log_handler):
+		self.App = app
+
 		# Add routes
 		webapp.router.add_get('/asab/v1/environ', self.environ)
 		webapp.router.add_get('/asab/v1/config', self.config)
@@ -14,6 +16,48 @@ class APIWebHandler(object):
 		webapp.router.add_get('/asab/v1/logws', log_handler.ws)
 
 		webapp.router.add_get('/asab/v1/changelog', self.changelog)
+
+		webapp.router.add_get('/asab/v1/metrics', self.metrics)
+
+	async def metrics(self, request):
+		metrics_service = self.App.get_service('asab.MetricsService')
+		print(request)
+		if metrics_service is None:
+			raise RuntimeError('asab.MetricsService is not available')
+
+		# lines = []
+		# lines.append('# TYPE asab_response summary')
+		# lines.append('# UNIT asab_response seconds')	
+
+		# for mname, metrics in metrics_service.Metrics.items():
+		# 	lines.append(metrics.build_openmetrics_line())
+
+		# lines.append('# EOF')
+
+		# text = '\n'.join(lines)
+		# print(request)
+		# return aiohttp.web.Response(text=text, content_type='text/plain')
+
+		for mname, metrics in metrics_service.Metrics.items():
+			if isinstance(metrics, asab.metrics.metrics.Counter):
+				print("counter!")
+				type = "counter"
+				counter_info = metrics.rest_get()
+				name = counter_info.get("Name")
+				tags = counter_info.get("Tags")
+				if "unit" in tags.keys():
+					unit = tags.get("unit")
+				for vname, value in counter_info.get("Values").items():
+					result_name = "_".join([name, vname])
+					lines = []
+					lines.append("# TYPE {} {}".format(result_name, type))
+					lines.append("# UNIT {} {}".format(result_name, unit))
+					lines.append("{} {}".format(result_name, value))
+					lines.append("# EOF")
+					text = '\n'.join(lines)
+					print(text)
+					return aiohttp.web.Response(text=text, content_type='text/plain')
+
 
 
 	async def changelog(self, request):
