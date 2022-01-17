092283349cab2074faca76fd2194da8bbdad7241
eliska <eliska.novotna@protonmail.com>
2022-01-17 17:23:39 +0100

    gauge added

--- asab/metrics/prometheus.py
+++ asab/metrics/prometheus.py
@@ -40,14 +40,13 @@ def get_labels(tags):
         return labels_str
 
 
-def counter_to_om(counter):
-    counter_lines = []
-    type = "counter"
-    m_name = counter.get("Name")
-    tags = counter.get("Tags")
+def metric_to_text(metric, type):
+    metric_lines = []
+    m_name = metric.get("Name")
+    tags = metric.get("Tags")
     labels_str = get_labels(tags)
 
-    for v_name, value in counter.get("Values").items():
+    for v_name, value in metric.get("Values").items():
         if validate_value is False:
             L.warning("Invalid OpenMetrics format. Value must be float or integer.")
             continue
@@ -59,35 +58,41 @@ def counter_to_om(counter):
                 unit = tags.get("unit")
                 unit = validate_format(unit)
                 name += "_{}".format(unit)
-                counter_lines.append("# TYPE {} {}".format(name, type))
-                counter_lines.append("# UNIT {} {}".format(name, unit))
+                metric_lines.append("# TYPE {} {}".format(name, type))
+                metric_lines.append("# UNIT {} {}".format(name, unit))
             else:
                 unit = None
-                counter_lines.append("# TYPE {} {}".format(name, type))
+                metric_lines.append("# TYPE {} {}".format(name, type))
                 L.warning("Invalid OpenMetrics format. Please, add 'unit' in 'Tags'.")
 
             if tags.get("help"):
                 help = tags.get("help")
-                counter_lines.append("# HELP {} {}".format(name, help))
+                metric_lines.append("# HELP {} {}".format(name, help))
             else:
                 L.warning("Invalid OpenMetrics format. Please, add 'help' in 'Tags'.")
 
+            if type == "counter":
+                metricpoint = "_total"
+            if type == "gauge":
+                metricpoint = ""
             if labels_str:
-                counter_lines.append("{}_total{} {}".format(name, labels_str, value))
+                metric_lines.append("{}{}{} {}".format(name, metricpoint, labels_str, value))
             else:
-                counter_lines.append("{}_total {}".format(name, value))
-    counter_text = '\n'.join(counter_lines)
-    return counter_text
+                metric_lines.append("{}{} {}".format(name, metricpoint, value))
+    metric_text = '\n'.join(metric_lines)
+    return metric_text
 
 
 def to_openmetrics(metrics_service):
     lines = []
     for mname, metrics in metrics_service.Metrics.items():
         if isinstance(metrics, asab.metrics.metrics.Counter):
-            print("counter!")
-            counter_info = metrics.rest_get()
-            counter_text = counter_to_om(counter_info)
+            counter_text = metric_to_text(metrics.rest_get(), type="counter")
             lines.append(counter_text)
+
+        if isinstance(metrics, asab.metrics.metrics.Gauge):
+            gauge_text = metric_to_text(metrics.rest_get(), type="gauge")
+            lines.append(gauge_text)
 
     lines.append("# EOF\n")
     text = '\n'.join(lines)
@@ -96,7 +101,7 @@ def to_openmetrics(metrics_service):
 
 # HOW TO FULLFIL OPEMETRICS STANDARD
 
-# ONLY Gauge and Counter are possible to translate into Prometheus. Other Metrics are omitted. 
+# ONLY Gauge and Counter translated into Prometheus. Other Metrics are omitted.
 # Metrics MUST have "unit" and "help" Tags
 # Help is a string and SHOULD be non-empty. It is used to give a brief description of the MetricFamily for human consumption and SHOULD be short enough to be used as a tooltip.
 # Metrics MUST have Lables - also added as items in Tags
@@ -105,12 +110,3 @@ def to_openmetrics(metrics_service):
 # Colons in MetricFamily names are RESERVED to signal that the MetricFamily is the result of a calculation or aggregation of a general purpose monitoring system. MetricFamily names beginning with underscores are RESERVED and MUST NOT be used unless specified by this standard. - Basically - anything that is not A-Z, a-z or digit is transformed into "_" and leading "_" is stripped.
 
 # Feel free to learn more about OpenMetrics standard from here: https://github.com/OpenObservability/OpenMetrics/blob/main/specification/OpenMetrics.md
-
-
-# TO DO
-# The combined length of the label names and values of an Exemplar's LabelSet MUST NOT exceed 128 UTF-8 characters. Other characters in the text rendering of an exemplar such as ",= are not included in this limit for implementation simplicity and for consistency between the text and proto formats.
-# A MetricPoint in a Metric's Counter's Total MAY reset to 0. If present, the corresponding Created time MUST also be set to the timestamp of the reset.
-# All exposers SHOULD be able to emit data secured with TLS 1.2 or later.
-# UTF-8 MUST be used. Byte order markers (BOMs) MUST NOT be used. As an important reminder for implementers, byte 0 is valid UTF-8 while, for example, byte 255 is not.
-# The content type MUST be: application/openmetrics-text; version=1.0.0; charset=utf-8
-# NaN is a number like any other in OpenMetrics, usually resulting from a division by zero such as for a summary quantile if there have been no observations recently. NaN does not have any special meaning in OpenMetrics, and in particular MUST NOT be used as a marker for missing or otherwise bad data.
--- test/test_prometheus.py
+++ test/test_prometheus.py
@@ -1,22 +1,22 @@
 import unittest
-from asab.metrics.prometheus import counter_to_om, get_labels, validate_format
+from asab.metrics.prometheus import metric_to_text, get_labels, validate_format
 
 
 class TestPrometheus(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestPrometheus, self).__init__(*args, **kwargs)
 
-    def test_counter_to_om(self):
+    def test_metric_to_text(self):
         input_counter = {'Name': 'mycounter', 'Tags': {'host': 'DESKTOP-6J7LEI1', 'unit': 'bytes', 'help': 'The most important counter ever.', 'label': 'sth', 'other_label': 'sth_else'}, 'Values': {'v1': 10, 'v2': 5}}
 
         expected_output = '# TYPE mycounter_v1_bytes counter\n# UNIT mycounter_v1_bytes bytes\n# HELP mycounter_v1_bytes The most important counter ever.\nmycounter_v1_bytes_total{label="sth",other_label="sth_else"} 10\n# TYPE mycounter_v2_bytes counter\n# UNIT mycounter_v2_bytes bytes\n# HELP mycounter_v2_bytes The most important counter ever.\nmycounter_v2_bytes_total{label="sth",other_label="sth_else"} 5'
-        output = counter_to_om(input_counter)
+        output = metric_to_text(input_counter, type="counter")
         self.assertEqual(expected_output, output)
 
         # missing labels and units
         input_counter2 = {'Name': 'mycounter', 'Tags': {'host': 'DESKTOP-6J7LEI1', 'help': 'The most important counter ever.'}, 'Values': {'v1': 10, 'v2': 5}}
         expected_output2 = '# TYPE mycounter_v1 counter\n# HELP mycounter_v1 The most important counter ever.\nmycounter_v1_total 10\n# TYPE mycounter_v2 counter\n# HELP mycounter_v2 The most important counter ever.\nmycounter_v2_total 5'
-        output2 = counter_to_om(input_counter2)
+        output2 = metric_to_text(input_counter2, type="counter")
         self.assertEqual(expected_output2, output2)
 
     def test_get_labels(self):
