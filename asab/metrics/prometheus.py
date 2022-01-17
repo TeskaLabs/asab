bc4d79a65c7c82aa6934de9c0dc33534cc3b96c3
eliska <eliska.novotna@protonmail.com>
2022-01-17 13:36:53 +0100

    labels tested

--- asab/metrics/prometheus.py
+++ asab/metrics/prometheus.py
@@ -1,39 +1,31 @@
 import asab
 
-		# lines = []
-		# lines.append('# TYPE asab_response summary')
-		# lines.append('# UNIT asab_response seconds')	
-
-		# for mname, metrics in metrics_service.Metrics.items():
-		# 	lines.append(metrics.build_openmetrics_line())
-
-		# lines.append('# EOF')
-
-		# text = '\n'.join(lines)
-		# print(request)
-		# return aiohttp.web.Response(text=text, content_type='text/plain')
-
-def counter_to_om(counter):
-    counter_lines = []
-    type = "counter"
-    
-    m_name = counter.get("Name")
-    tags = counter.get("Tags")
-    
-    labels = {}
+def get_labels(tags):
+    labels_str = "{"
     for tag in tags.keys():
         if tag == "host" or tag == "unit" or tag == "help":
             continue
         else:
-            labels[tag] = tags.get(tag)
-    labels_str = "{"
-    for l_name, label in labels.items():
-        labels_str += '{}="{}",'.format(l_name, label)
+            labels_str += '{}="{}",'.format(tag, tags.get(tag))
     labels_str = labels_str.rstrip(",")
     labels_str += "}"
+    if labels_str == "{}":
+        return None
+    else:
+        return labels_str
+
+
+
+def counter_to_om(counter):
+    counter_lines = []
+    type = "counter"
+    m_name = counter.get("Name")
+    tags = counter.get("Tags")
+    labels_str = get_labels(tags)
     
     for v_name, value in counter.get("Values").items():
         name = "_".join([m_name, v_name])
+
         # If a unit is specified it MUST be provided in a UNIT metadata line. In addition, an underscore and the unit MUST be the suffix of the MetricFamily name.
         if tags.get("unit"):
             unit = tags.get("unit")
@@ -48,10 +40,10 @@ def counter_to_om(counter):
             help = tags.get("help")
             counter_lines.append("# HELP {} {}".format(name, help))
 
-        if labels == {}:
-            counter_lines.append("{}_total {}".format(name, value))
-        else:
+        if labels_str:
             counter_lines.append("{}_total{} {}".format(name, labels_str, value))
+        else:
+            counter_lines.append("{}_total {}".format(name, value))
     counter_text = '\n'.join(counter_lines)
     return counter_text
 
