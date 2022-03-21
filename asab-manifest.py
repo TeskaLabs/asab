40ea496954dc198c85fed237decf9945f04248b7
Ales Teska <ales.teska@teskalabs.com>
2022-03-21 16:25:09 +0100

    Initial work on the manifest creation.

---
+++ asab-manifest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import json
+
+
+def create_manifest():
+	manifest = {
+		# TODO: ...
+	}
+
+	with open("MANIFEST.json", "w") as f:
+		json.dump(manifest, f)
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
