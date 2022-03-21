59a8cddd6bb61f2c9773c752e4cf18fa31869dd4
Ales Teska <ales.teska@teskalabs.com>
2022-03-21 16:45:24 +0100

    Progress on the MANIFEST.json structure.

--- asab-manifest.py
+++ asab-manifest.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
 import json
+import argparse
+import datetime
+import subprocess
 
 
-def create_manifest():
+def create_manifest(args):
 	manifest = {
-		# TODO: ...
+		'created_at': datetime.datetime.utcnow().isoformat() + 'Z',
 	}
 
-	with open("MANIFEST.json", "w") as f:
-		json.dump(manifest, f)
+	gitr = subprocess.run(["git", "describe", "--abbrev=7", "--tags", "--dirty", "--always"], capture_output=True)
+	if gitr.returncode == 0:
+		manifest['version'] = gitr.stdout.decode('ascii').strip()
+	else:
+		pass	
+		# TODO: Print warning if returncode is not 0
+
+	with open(args.manifest, "w") as f:
+		json.dump(manifest, f, indent='\t')
+
+	print("{} created successfully".format(args.manifest))
 
 
 def main():
-	pass
+	parser = argparse.ArgumentParser(description='Create MANIFEST.json file')
+	parser.add_argument('manifest', metavar='MANIFEST', type=str, help='Location of the MANIFEST.json file')
+
+	args = parser.parse_args()
+	create_manifest(args)
 
 
 if __name__ == '__main__':
