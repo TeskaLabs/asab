d598e962b2f45448d1b4071bc3ddb4c5ca675add
eliska <eliska.novotna@protonmail.com>
2022-03-31 12:51:23 +0200

    columns width fixed

--- asab/metrics/prometheus.py
+++ asab/metrics/prometheus.py
@@ -2,7 +2,6 @@ import asab
 
 
 class PrometheusTarget(asab.ConfigObject):
-
 	def __init__(self, svc, config_section_name, config=None):
 		super().__init__(config_section_name, config)
 		self.mlist = None
@@ -20,7 +19,7 @@ class PrometheusTarget(asab.ConfigObject):
 				if record:
 					lines.append(record)
 			lines.append("# EOF\n")
-			text = '\n'.join(lines)
+			text = "\n".join(lines)
 			return text
 
 	def watch_table(self, request):
@@ -28,24 +27,55 @@ class PrometheusTarget(asab.ConfigObject):
 		lines = []
 		if self.mlist:
 			m_name_len = max([len(metric.Name) for metric, values in self.mlist])
-			print(m_name_len)
-				
+			v_name_len = max(
+				[
+					len(str(value_name))
+					for metric, values in self.mlist
+					for value_name in values.keys()
+				]
+			)
+
 			if agg == "sum":
-				lines.append("-" * 60)
-				lines.append("{:<{m_name_len}} {:<30}".format("Metric name", "Metric values SUM", m_name_len=m_name_len))
-				lines.append("-" * 60)
+				separator = "-" * (m_name_len + 30 + 2)
+				lines.append(separator)
+				lines.append(
+					"{:<{m_name_len}} | {:<30}".format(
+						"Metric name", "Metric values SUM", m_name_len=m_name_len
+					)
+				)
+				lines.append(separator)
 				for metric, values in self.mlist:
 					name = metric.Name
-					_sum = sum(values.values())
-					print(_sum)
-					lines.append("{:<{m_name_len}} {:<30}".format(str(name), sum(values.values()), m_name_len=m_name_len))
+					lines.append(
+						"{:<{m_name_len}} | {:<30}".format(
+							str(name), sum(values.values()), m_name_len=m_name_len
+						)
+					)
+
 			else:
-				lines.append("-" * 90)
-				lines.append("{:<30} {:<30} {:<30}".format("Metric name", "Value name", "Value"))
-				lines.append("-" * 90)
+				separator = "-" * (m_name_len + v_name_len + 30 + 2)
+				lines.append(separator)
+				lines.append(
+					"{:<{m_name_len}} | {:<{v_name_len}} | {:<30}".format(
+						"Metric name",
+						"Value name",
+						"Value",
+						v_name_len=v_name_len,
+						m_name_len=m_name_len,
+					)
+				)
+				lines.append(separator)
 				for metric, values in self.mlist:
 					name = metric.Name
 					for value_name, value in values.items():
-						lines.append("{:<30} {:<30} {:<30}".format(str(name), str(value_name), str(value)))
-		text = '\n'.join(lines)
+						lines.append(
+							"{:<{m_name_len}} | {:<{v_name_len}} | {:<30}".format(
+								str(name),
+								str(value_name),
+								str(value),
+								v_name_len=v_name_len,
+								m_name_len=m_name_len,
+							)
+						)
+		text = "\n".join(lines)
 		return text
