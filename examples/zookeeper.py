82f10f3a93ad5d12f49ad688ef18573d037afc9d
mshivashankar <mithun.shivashankar@teskalabs.com>
2022-05-05 16:16:49 +0200

    Add todo

--- examples/zookeeper.py
+++ examples/zookeeper.py
@@ -12,15 +12,23 @@ class MyApplication(asab.Application):
 	def __init__(self):
 		super().__init__()
 
+	async def main(self):
+		# TODO
+		"""
 		# Loading the zookeeper service module
 		self.add_module(asab.zookeeper.Module)
-
-		# Advertise self thru ZooKeeper
 		zksvc = self.get_service("asab.ZooKeeperService")
-		zksvc.DefaultContainer
+		zk_cont = zksvc.DefaultContainer
+		zk_clinet = zk_cont.ZooKeeper
+		zk_path = zk_cont.ZooKeeperPath
+		zk_path = zk_path + "/Zookeeper_example"
+		await zk_clinet.ensure_path(zk_path)
+		await zk_clinet.set_data(zk_path, "Hello world!")
+		await print(zk_clinet.get_data(zk_path))
+
+	"""
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app = MyApplication()
 	app.run()
