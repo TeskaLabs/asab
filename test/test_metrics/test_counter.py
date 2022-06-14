1e9830ee6219e0620a7a4fad0794ec816c5f73cc
eliska <eliska.novotna@protonmail.com>
2022-06-14 16:20:45 +0200

    unit and help in counter tested

--- test/test_metrics/test_counter.py
+++ test/test_metrics/test_counter.py
@@ -72,7 +72,7 @@ class TestCounter(MetricsTestCase):
 			])
 		)
 
-		# Test OpenMetric output with init values
+		# Test OpenMetric output after flush
 		om_format = asab.metrics.openmetric.metric_to_openmetric(my_counter.Storage)
 		self.assertEqual(
 			om_format,
@@ -80,5 +80,54 @@ class TestCounter(MetricsTestCase):
 				'# TYPE mycounter gauge\n',
 				'mycounter{foo="bar",host="mockedhost.com",value_name="value1"} 1\n',
 				'mycounter{foo="bar",host="mockedhost.com",value_name="value2"} 0',
+			])
+		)
+
+
+	def test_counter_02(self):
+		my_counter = self.MetricsService.create_counter(
+			"mycounter",
+			tags={'foo': 'bar'},
+			init_values={'value1': 0, 'value2': 0},
+			help="Help!",
+			unit="ages",
+		)
+
+		# Test InfluxDB output with init values
+		influxdb_format = asab.metrics.influxdb.influxdb_format(self.MetricsService.Storage.Metrics, 123.45)
+		self.assertEqual(
+			influxdb_format,
+			''.join([
+				"mycounter,foo=bar,host=mockedhost.com value1=0i 123450000000\n",
+				"mycounter,foo=bar,host=mockedhost.com value2=0i 123450000000\n",
+			])
+		)
+
+		# Test OpenMetric output with init values
+		om_format = asab.metrics.openmetric.metric_to_openmetric(my_counter.Storage)
+		self.assertEqual(
+			om_format,
+			''.join([
+				'# TYPE mycounter_ages gauge\n',
+				'# UNIT mycounter_ages ages\n',
+				'# HELP mycounter_ages Help!\n',
+				'mycounter_ages{foo="bar",host="mockedhost.com",value_name="value1"} 0\n',
+				'mycounter_ages{foo="bar",host="mockedhost.com",value_name="value2"} 0',
+			])
+		)
+
+		my_counter.add('value1', 1)
+		self.MetricsService._flush_metrics()
+
+		# Test OpenMetric output after flush
+		om_format = asab.metrics.openmetric.metric_to_openmetric(my_counter.Storage)
+		self.assertEqual(
+			om_format,
+			''.join([
+				'# TYPE mycounter_ages gauge\n',
+				'# UNIT mycounter_ages ages\n',
+				'# HELP mycounter_ages Help!\n',
+				'mycounter_ages{foo="bar",host="mockedhost.com",value_name="value1"} 1\n',
+				'mycounter_ages{foo="bar",host="mockedhost.com",value_name="value2"} 0',
 			])
 		)
