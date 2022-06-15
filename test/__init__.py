a818ab3e696f2e4ebd495ccb3816ff82eff207e9
Ales Teska <ales.teska@teskalabs.com>
2022-06-16 01:29:53 +0200

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -2,6 +2,6 @@ from .test_metrics import *
 from .test_metrics.test_counter import *
 from .test_metrics.test_gauge import *
 from .test_metrics.test_eps_counter import *
-from .test_agg_counter import *
+from .test_metrics.test_agg_counter import *
 from .test_metrics.test_histogram import *
 
