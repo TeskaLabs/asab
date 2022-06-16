362b466761285bb554588079f8c1fdf344915d55
eliska <eliska.novotna@protonmail.com>
2022-06-16 18:08:20 +0200

    better tests

--- test/test_metrics/test_duty_cycle.py
+++ test/test_metrics/test_duty_cycle.py
@@ -40,6 +40,57 @@ class TestDutyCycle(MetricsTestCase):
 			expectation,
 		)
 
+
+	def test_dutycycle_01(self):
+		"""
+		Storage wire-format
+		dutycycle True
+		"""
+		my_dutycycle = self.MetricsService.create_duty_cycle(
+			self.MockedLoop,
+			"testdc",
+			init_values={"v1": True}
+		)
+
+		now = 124.45
+		my_dutycycle.flush(now)
+
+		expectation = {
+			"name": "testdc",
+			"type": "DutyCycle",
+			"fieldset": [{
+				"tags": {
+					"host": "mockedhost.com",
+				},
+				"actuals": {
+					"v1": {
+						"on_off": True,
+						"timestamp": 124.45,
+						"off_cycle": 0.0,
+						"on_cycle": 0.0,
+					}
+				},
+				"values": {"v1": 1.0}
+			}
+			]
+		}
+
+		self.assertDictEqual(
+			my_dutycycle.Storage,
+			expectation,
+		)
+
+	def test_dutycycle_02(self):
+		"""
+		Storage wire-format
+		dutycycle False
+		"""
+		my_dutycycle = self.MetricsService.create_duty_cycle(
+			self.MockedLoop,
+			"testdc",
+			init_values={"v1": True}
+		)
+
 		my_dutycycle.set("v1", False)
 		now = 124.45
 		my_dutycycle.flush(now)
@@ -68,27 +119,3 @@ class TestDutyCycle(MetricsTestCase):
 			my_dutycycle.Storage,
 			expectation,
 		)
-
-		my_dutycycle.set("v1", True)
-		now = 124.45
-		my_dutycycle.flush(now)
-
-		expectation = {
-			"name": "testdc",
-			"type": "DutyCycle",
-			"fieldset": [{
-				"tags": {
-					"host": "mockedhost.com",
-				},
-				"actuals": {
-					"v1": {
-						"on_off": False,
-						"timestamp": 124.45,
-						"off_cycle": 0.0,
-						"on_cycle": 0.0,
-					}
-				},
-				"values": {"v1": 1.0}
-			}
-			]
-		}
