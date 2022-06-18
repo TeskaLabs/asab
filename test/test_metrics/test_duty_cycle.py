442ec5aebe32afc980b778d4b91d822552ecfe76
eliska <eliska.novotna@protonmail.com>
2022-06-18 14:27:13 +0200

    dutycycle could be well taken from both promeheus and influx targets

--- test/test_metrics/test_duty_cycle.py
+++ test/test_metrics/test_duty_cycle.py
@@ -119,3 +119,49 @@ class TestDutyCycle(MetricsTestCase):
 			my_dutycycle.Storage,
 			expectation,
 		)
+
+	def test_dutycycle_03(self):
+		"""
+		InfluxDB
+		"""
+		my_dutycycle = self.MetricsService.create_duty_cycle(
+			self.MockedLoop,
+			"testdc",
+			init_values={"v1": True}
+		)
+
+		now = 124.45
+		my_dutycycle.flush(now)
+
+		# Test Influx format with init values
+		influx_format = asab.metrics.influxdb.influxdb_format(self.MetricsService.Storage.Metrics, 123.45)
+		self.assertEqual(
+			influx_format,
+			''.join([
+				'testdc,host=mockedhost.com v1=1.0 123450000000\n',
+			])
+		)
+
+
+	def test_dutycycle_04(self):
+		"""
+		OpenMetric
+		"""
+		my_dutycycle = self.MetricsService.create_duty_cycle(
+			self.MockedLoop,
+			"testdc",
+			init_values={"v1": True}
+		)
+
+		now = 124.45
+		my_dutycycle.flush(now)
+
+		# Test OpenMetric output with init values
+		om_format = asab.metrics.openmetric.metric_to_openmetric(my_dutycycle.Storage)
+		self.assertEqual(
+			om_format,
+			''.join([
+				'# TYPE testdc gauge\n',
+				'testdc{host="mockedhost.com",name="v1"} 1.0',
+			])
+		)
