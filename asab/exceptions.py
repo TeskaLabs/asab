572263ec5c8e991c91b8c07c59fc031a20c6f927
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2022-06-21 11:45:20 +0200

    Exceptions: Add ValidationError and Conflict

---
+++ asab/exceptions.py
@@ -0,0 +1,23 @@
+class ValidationError(Exception):
+	pass
+
+
+class Conflict(Exception):
+	# TODO: Handle when the value of `key` or `value` is `None`
+	def __init__(self, message=None, *args, key=None, value=None):
+		self.Key = key
+		self.Value = value
+
+		if message is None:
+			if key is not None:
+				if value is not None:
+					message = "in field {}: {}".format(repr(key), repr(value))
+				else:
+					message = "in field {}".format(repr(key))
+			elif value is not None:
+				message = value
+
+		if message is None:
+			super().__init__(*args)
+		else:
+			super().__init__(message, *args)
