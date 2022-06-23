1965821626419cf37db289335afbd8caa824cd07
Ales Teska <ales.teska@teskalabs.com>
2022-06-23 19:02:36 +0200

    Progress on metrics duplicates.

--- test/test_metrics/test_duplicates.py
+++ test/test_metrics/test_duplicates.py
@@ -4,8 +4,5 @@ from .baseclass import MetricsTestCase
 class TestDuplicates(MetricsTestCase):
 
 	def test_duplicates_01(self):
-		try:
-			Gauge1 = self.MetricsService.create_gauge("gauge1", init_values={"v1": 0})
-		finally:
-			pass
+		Gauge1 = self.MetricsService.create_gauge("gauge1", init_values={"v1": 0})
 		Gauge2 = self.MetricsService.create_gauge("gauge1", init_values={"v1": 0})
