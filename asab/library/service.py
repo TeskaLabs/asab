ffad799ec78d99e3eb191b4d304b02ed534ab902
mshivashankar <mithun.shivashankar@teskalabs.com>
2022-07-01 08:52:59 +0200

    Paraphrase comment

--- asab/library/service.py
+++ asab/library/service.py
@@ -65,10 +65,10 @@ class LibraryService(asab.Service):
 			When tenant='xxxxx'
 			The method returns list of yaml files that are disabled for tenant 'xxxxx'.
 
-			When recursive = TRUE
+			When recursive=True
 			returns a list of yaml files located in zero or more directories and
 			subdirectories.
-			When recursive = False
+			When recursive=False
 			returns a list of yaml files located in /library.
 		"""
 		for library in self.Libraries.values():
