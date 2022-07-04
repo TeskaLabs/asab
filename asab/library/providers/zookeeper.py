d77dde22f4d4af60d353806686b837f810ef65f8
Ales Teska <ales.teska@teskalabs.com>
2022-07-04 20:56:07 +0200

    Better exception handling.

--- asab/library/providers/zookeeper.py
+++ asab/library/providers/zookeeper.py
@@ -2,6 +2,7 @@ import logging
 import os.path
 import urllib.parse
 
+import kazoo.exceptions
 import yaml
 
 from .abc import LibraryProviderABC
@@ -62,19 +63,28 @@ class ZooKeeperLibraryProvider(LibraryProviderABC):
 	async def read(self, path):
 		if self.Zookeeper is None:
 			L.warning("Zookeeper Client has not been established (yet). Cannot read {}".format(path))
-			return
+			return None
+		
 		node_path = "{}/{}".format(self.BasePath, path)
-		node_data = await self.Zookeeper.get_data(node_path)
 
-		return node_data.decode('utf-8')
+		try:
+			node_data = await self.Zookeeper.get_data(node_path)
+		except kazoo.exceptions.NoNodeError:
+			return None
+
+		# Consider adding other exceptions from Kazoo to indicate common non-critical errors
+
+		return node_data
 
 
 	async def list(self, path, tenant=None, recursive=True):
 		if self.Zookeeper is None:
 			L.warning("Zookeeper Client has not been established (yet). Cannot list {}".format(path))
-			return
+			return None
+
 		node_names = list()
 		node_path = self.create_zookeeper_path(path1=path)
+
 		await self._list_by_node_path(node_path, node_names, tenant, recursive)
 		return node_names
 
