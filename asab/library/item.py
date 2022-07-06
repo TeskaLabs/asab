ae358f83e21a2884ee206477f3043f3188e25922
Ales Teska <ales.teska@teskalabs.com>
2022-07-06 11:45:58 +0200

    Smaller documentation improvement.

--- asab/library/item.py
+++ asab/library/item.py
@@ -10,12 +10,12 @@ class LibraryItem:
 	It MUST start with `/` and contains the whole absolute path.
 	The `name` can be directly fed into `LibraryService.read(...)`.
 
-	`type` is `item` or `dir`
+	* `type` is `item` or `dir`
 
-	`providers` is a list of providers that provide this item.
+	* `providers` is a list of providers that provide this item.
 	`dir` LibaryItems can be provided by more than one provider.
 
-	`disabled` if True, then this item is disabled. `LibraryService.read(...)` will return `None`.
+	* `disabled` if True, then this item is disabled. `LibraryService.read(...)` will return `None`.
 	'''
 
 	name: str
