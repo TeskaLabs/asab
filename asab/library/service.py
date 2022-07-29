5dde8f2a1de09759e8dca5214c571467b68330c9
mshivashankar <mithun.shivashankar@teskalabs.com>
2022-07-29 15:00:50 +0300

    do on tick 60 seconds

--- asab/library/service.py
+++ asab/library/service.py
@@ -86,7 +86,7 @@ class LibraryService(Service):
 
 		for path in paths:
 			self._create_library(path)
-		app.PubSub.subscribe("Application.tick!", self.on_tick)
+		app.PubSub.subscribe("Application.tick/60!", self.on_tick)
 
 
 	async def finalize(self, app):
