3ae258b83ebbc10d51267f1cae52820ab5d93540
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2023-02-14 16:25:53 +0100

    Public key initialization

--- asab/web/authz/service.py
+++ asab/web/authz/service.py
@@ -1,4 +1,5 @@
 import base64
+import binascii
 import json
 import logging
 import aiohttp
@@ -19,8 +20,8 @@ L = logging.getLogger(__name__)
 
 asab.Config.add_defaults({
 	"authz": {
-		"public_keys_url": "http://localhost:8081/.well_known/jwks.json",  # If set to "", ID tokens are not validated
-		"cache_expiration": "1 m"
+		"public_keys_url": "",
+		"_disable_token_verifiction": "no",
 	}
 })
 
@@ -29,32 +30,26 @@ class AuthzService(asab.Service):
 
 	def __init__(self, app, service_name="asab.AuthzService"):
 		super().__init__(app, service_name)
+		self._DisableTokenVerification = asab.Config.getboolean("authz", "_disable_token_verifiction")
 		self.PublicKeysUrl = asab.Config.get("authz", "public_keys_url")
-		self.PublicKey = None
-		if self.PublicKeysUrl is not None and len(self.PublicKeysUrl) != 0:
-			self.ValidationEnabled = True
-		else:
-			L.warning("No public_keys_url provided! Authenticity of incoming ID tokens cannot be verified!")
-			self.ValidationEnabled = False
+		if len(self.PublicKeysUrl) == 0 and not self._DisableTokenVerification:
+			raise ValueError("No public_keys_url provided in [authz] section.")
+		self.PublicKey = None  # TODO: Support multiple public keys
+		self.App.PubSub.subscribe("Application.tick/10!", self.initialize)
 
-	async def initialize(self, app):
-		if self.PublicKeysUrl is not None:
-			# TODO: Retry if unsuccessful
-			await self.get_public_keys()
 
-	async def get_public_keys(self):
-		async with aiohttp.ClientSession() as session:
-			async with session.get(self.PublicKeysUrl) as response:
-				if response.status != 200:
-					L.error("Cannot retrieve public keys from authorization server", struct_data={
-						"status": response.status,
-						"url": self.PublicKeysUrl,
-						"text": await response.text(),
-					})
-					raise ConnectionError()
+	async def initialize(self, *args, **kwargs):
+		if not self.is_ready():
+			await self._get_public_keys()
 
-				data = await response.json()
-				self.PublicKey = jwcrypto.jwk.JWK(**data["keys"].pop())
+
+	def is_ready(self):
+		if self._DisableTokenVerification is True:
+			return True
+		elif self.PublicKey is not None:
+			return True
+		return False
+
 
 	async def authorize(self, resources, bearer_token, tenant=None):
 		# Use userinfo to make RBAC check
@@ -89,15 +84,54 @@ class AuthzService(asab.Service):
 
 		return True
 
+
 	async def userinfo(self, bearer_token):
-		return self._get_id_token_claims(bearer_token)
+		if not self.is_ready():
+			raise Exception("AuthzService is not ready: No public keys loaded yet.")
 
-	def _get_id_token_claims(self, bearer_token):
-		if not self.ValidationEnabled:
-			# TODO: Safer deserialization
-			return json.loads(base64.b64decode(bearer_token.split(".")[1].encode("utf-8"), validate=False))
+		if self._DisableTokenVerification:
+			return self._get_id_token_claims_without_verification(bearer_token)
+		else:
+			return self._get_id_token_claims(bearer_token)
 
-		# TODO: Don't check signature if no public key url is provided
+
+	async def _get_public_keys(self):
+		async with aiohttp.ClientSession() as session:
+			async with session.get(self.PublicKeysUrl) as response:
+				if response.status != 200:
+					L.error("Error retrieving public keys from authorization server.", struct_data={
+						"status": response.status,
+						"url": self.PublicKeysUrl,
+						"text": await response.text(),
+					})
+					return
+				try:
+					data = await response.json()
+				except json.JSONDecodeError:
+					L.error("Authorization server response cannot be parsed as JSON.", struct_data={
+						"url": self.PublicKeysUrl,
+						"data": data,
+					})
+					return
+				try:
+					key_data = data["keys"].pop()
+				except (IndexError, KeyError):
+					L.error("Authorization server response contains no public keys.", struct_data={
+						"url": self.PublicKeysUrl,
+						"data": data,
+					})
+					return
+				try:
+					self.PublicKey = jwcrypto.jwk.JWK(**key_data)
+				except IndexError:
+					L.error("Error reading JSON Web Key.", struct_data={
+						"url": self.PublicKeysUrl,
+						"data": data,
+					})
+					return
+
+
+	def _get_id_token_claims(self, bearer_token: str):
 		try:
 			token = jwcrypto.jwt.JWT(jwt=bearer_token, key=self.PublicKey)
 		except jwcrypto.jwt.JWTExpired:
@@ -113,3 +147,19 @@ class AuthzService(asab.Service):
 			raise asab.exceptions.NotAuthenticatedError("Cannot parse ID token claims.")
 
 		return token_claims
+
+
+	def _get_id_token_claims_without_verification(self, bearer_token: str):
+		try:
+			header, payload, signature = bearer_token.split(".")
+		except IndexError:
+			raise asab.exceptions.NotAuthenticatedError("Cannot parse ID token: Wrong number of '.'.")
+
+		try:
+			claims = json.loads(base64.b64decode(payload.encode("utf-8")))
+		except binascii.Error:
+			raise asab.exceptions.NotAuthenticatedError("Cannot parse ID token: Payload is not base 64.")
+		except json.JSONDecodeError:
+			raise asab.exceptions.NotAuthenticatedError("Cannot parse ID token: Payload cannot be parsed as JSON.")
+
+		return claims
