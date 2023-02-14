c47c08b8aeffed7572ad05a8f357079af7f2134d
Ales Teska <ales.teska@teskalabs.com>
2023-02-14 21:11:26 +0100

    Tab to spaces (in tutorial).

--- doc/tutorial/web/web1/app.py
+++ doc/tutorial/web/web1/app.py
@@ -4,23 +4,23 @@ import asab.web.rest
 
 class MyWebApplication(asab.Application):
 
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		# Create the Web server
-		web = asab.web.create_web_server(self)
+        # Create the Web server
+        web = asab.web.create_web_server(self)
 
-		# Add a route to the handler method
-		web.add_get('/hello', self.hello)
+        # Add a route to the handler method
+        web.add_get('/hello', self.hello)
 
-	# This is the web request handler
-	async def hello(self, request):
-		return asab.web.rest.json_response(
-			request,
-			data="Hello, world!\n"
-		)
+    # This is the web request handler
+    async def hello(self, request):
+        return asab.web.rest.json_response(
+            request,
+            data="Hello, world!\n"
+        )
 
 
 if __name__ == '__main__':
-	app = MyWebApplication()
-	app.run()
+    app = MyWebApplication()
+    app.run()
