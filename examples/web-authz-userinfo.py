eae5fe4fbef9254e11e87165e1d34d2bc1db2809
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2023-02-15 18:58:47 +0100

    Update example instructions

--- examples/web-authz-userinfo.py
+++ examples/web-authz-userinfo.py
@@ -15,22 +15,43 @@ L = logging.getLogger(__name__)
 
 class MyApplication(asab.Application):
 	"""
-	MyApplication serves endpoints which may use user info obtained from the authentication server.
-
-	Run this app with config file:
-	```sh
-	python3 examples/web-authz-userinfo.py -c examples/web-authz-userinfo.conf
-	```
-
-	TODO:
-		- Add protected ngingx location with introspection!
-		- Register client!
-		- Set up introspection!
+	MyApplication serves endpoints which use user info obtained from ID tokens issued by the authorization server.
 
 	Test by:
-	1) Run SeaCat Auth at: http://localhost:8081
-	2) Perform OAuth authentication to obtain access token
-	3) Run: curl -H "Authorization: <ACCESS_TOKEN>" http://localhost:8089/user
+	1) Run NGINX server with Seacat Auth (at localhost:8081)
+	2) Run this example app with config file:
+		```sh
+		python3 examples/web-authz-userinfo.py -c examples/web-authz-userinfo.conf
+		```
+	1) In Seacat Admin UI, register a public web client for this app, with client_id="example-app"
+	3) Set up a NGINX location with intropection for this example app:
+		```nginx
+		location /example {
+			rewrite ^/example/(.*)? /$1 break;
+			proxy_pass http://localhost:8089;
+
+			auth_request /_example_cookie_introspect;
+			error_page 401 /auth/api/openidconnect/authorize?response_type=code&scope=openid%20cookie%20profile&client_id=example-app&redirect_uri=$request_uri;
+
+			auth_request_set      $authorization $upstream_http_authorization;
+			proxy_set_header      Authorization $authorization;
+
+			auth_request_set      $cookie $upstream_http_cookie;
+			proxy_set_header      Cookie $cookie;
+
+			auth_request_set   $set_cookie $upstream_http_set_cookie;
+			add_header	Set-Cookie $set_cookie;
+		}
+
+		location = /_example_cookie_introspect {
+			internal;
+			proxy_method          POST;
+			proxy_set_body        "$http_authorization";
+			proxy_pass            http://auth_api/cookie/nginx?client_id=example-app;
+			proxy_ignore_headers  Cache-Control Expires Set-Cookie;
+		}
+		```
+	4) Access "https://YOUR_DOMAIN/example/user" in your browser
 	"""
 
 	async def initialize(self):
