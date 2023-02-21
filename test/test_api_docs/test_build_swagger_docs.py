99ddddaa4a27ec2cae73094f785656ad36c81c5f
mejroslav <miroslav.burysek@gmail.com>
2023-02-21 10:20:27 +0100

    fix: expected_swagger_output keys

--- test/test_api_docs/test_build_swagger_docs.py
+++ test/test_api_docs/test_build_swagger_docs.py
@@ -13,7 +13,7 @@ class TestSwagger(TestDocWebHandler):
 		self.assertTrue(check_keys(documentation, keys))
 
 	def test_all_keys(self):
-		"""Documentation should contain the same keys as a dictionary in `swagger_template.py`"""
+		"""Documentation should contain the same keys as a dictionary in `expected_swagger_output.py`"""
 		documentation: dict = self.handlerObject.build_swagger_documentation()
 		print("Test documentation was generated. The corresponding object is:\n{0}".format(documentation))
 		self.assertTrue(compare_keys(documentation, TEST_ALL_KEYS_EXPECTED_OUTPUT))
