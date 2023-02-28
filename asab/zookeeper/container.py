5276722443b67ab12bdc7f438d8b9e275c4561f3
Ales Teska <ales.teska@teskalabs.com>
2023-02-28 18:02:51 +0100

    Zookeeper integration refactored.

--- asab/zookeeper/container.py
+++ asab/zookeeper/container.py
@@ -1,6 +1,8 @@
 import json
 import asyncio
 import logging
+import urllib.parse
+import configparser
 
 import kazoo.exceptions
 import kazoo.recipe.watchers
@@ -21,36 +23,126 @@ class ZooKeeperContainer(ConfigObject):
 	ConfigDefaults = {
 		# Server list to which ZooKeeper Client tries connecting.
 		# Specify a comma (,) separated server list.
-		# A server is defined as address:port format.
+		# A server is defined as `address:port` format.
 		# Example:
 		# "servers": "zookeeper-1:2181,zookeeper-2:2181,zookeeper-3:2181"
-		#
-		# WARNING: If the value is not provided (empty string), Zookeeper container refuses to start
+		# If servers are empty, default from [zookeeper]/servers will be taken
+		# If [zookeeper]/servers value is not provided, ZOOKEEPER_SERVERS environment variable is used
 		"servers": "",
 
-		# The default is an application class name
+		# If not provided, "/asab" path will be used
 		"path": "",
 	}
 
 
 	def __init__(self, zksvc, config_section_name, config=None, z_path=None):
 		"""
-		Alternative 1: Obtain Zookeeper container with `config_section_name` configuration section
-		Alternative 2: Obtain Zookeeper container with call z_path (URL)
-		Example:
-		zk_cnt = ZooKeeperContainer(app, config_section_name='', z_path=z_path)
+		Zookeeper cofiguration:
+
+		* Source 1: Obtain Zookeeper configuration from the configuration section specified by `config_section_name` argument
+
+		* Source 2: Obtain Zookeeper configuration from `z_path` argument, which is Zookeeper URL (see below)
+
+		* Source 3: `ZOOKEEPER_SERVERS` environment variable
+
+
+		The `z_path` argument has precedence over config but the implementation will look
+		at configuration if `z_path` URL is missing completely or partially.
+
+		Supported types of z_path` URLs:
+
+		1. Absolute URL
+			Example: zookeeper://zookeeper:12181/etc/configs/file1
+
+			There is no fallback to the configuration.
+
+
+
+		2. Relative URL with full path
+			Example: zookeeper:///etc/configs/file1
+			
+			In this case the relative url is expanded as follows:
+			zookeeper://{zookeeper_server}/etc/configs/file1
+			
+			Where {zookeeper_server} is substituted with the server entry of the [zookeeper] configuration file section.
+
+
+
+		3. Relative URL with relative path
+		
+			Example: zookeeper:./etc/configs/file1
+				In this case, the relative URL is expanded as follows:
+				
+				zookeper://{zookeeper_server}/{zookeeper_path}/etc/configs/file1
+				Where {zookeeper_server} is substituted with the "server" entry of the [zookeeper] configuration file section and
+				{zookeeper_path} is substituted with the "path" entry of the [zookeeper] configuration file section.
+
+		Sample config file:
+		[zookeeper]
+		server=server1:port1,server2:port2,server3:port3
+		path=myfolder
+
 		"""
 		super().__init__(config_section_name=config_section_name, config=config)
-
 		self.App = zksvc.App
-		self.ConfigSectionName = config_section_name
-		self.ZooKeeper = KazooWrapper(zksvc.App, self.Config, z_path)
-		self.ZooKeeperPath = self.ZooKeeper.Path
+
+		# Parse URL from z_path
+		if z_path is not None:
+			url_pieces = urllib.parse.urlparse(z_path)
+			url_netloc = url_pieces.netloc
+			url_path = url_pieces.path
+		else:
+			url_netloc = ""
+			url_path = ""
+
+		# If there is no location, use the value of 'servers' from the configuration
+		if url_netloc == "":
+			url_netloc = self.Config.get("servers", "")
+		
+		if url_netloc == "":
+			from ..config import Config
+			try:
+				url_netloc = Config.get("zookeeper", "servers")
+			except (configparser.NoOptionError, configparser.NoSectionError):
+				pass
+		
+		if url_netloc == "":
+			url_netloc = os.environ.get("ZOOKEEPER_SERVERS", "")
+
+		if url_netloc == "":
+			# if server entry is missing exit
+			L.critical("Cannot connect to Zookeeper, the configuration of the server address is not available.")
+			raise SystemExit("Exit due to a critical configuration error.")
+
+		assert url_netloc is not None
+
+		# If path has not been provided, use the value of 'path' from the configuration
+		if url_path == "":
+			url_path = self.Config.get("path", "")
+
+		if url_path == "":
+			from ..config import Config
+			try:
+				url_path = Config.get("zookeeper", "path")
+			except (configparser.NoOptionError, configparser.NoSectionError):
+				pass
+
+		if url_path == "":
+			url_path = 'asab'
+
+		# Remove all heading '/' from path
+		while url_path.startswith('/'):
+			url_path = url_path[1:]
+
+		self.ZooKeeper = KazooWrapper(zksvc, url_netloc)
+		self.Path = url_path
+		
 		self.Advertisments = dict()
 		self.DataWatchers = set()
-		self.App.PubSub.subscribe("Application.tick/300!", self._do_advertise)
+		self.App.PubSub.subscribe("Application.tick/300!", self._readvertise)
 
-		zksvc._register_container(self)
+		zksvc.Containers.append(self)
+		zksvc.ProactorService.schedule(self._start, self.App)
 
 
 	def _start(self, app):
@@ -66,11 +158,10 @@ class ZooKeeperContainer(ConfigObject):
 			)
 			return
 
-		self.ZooKeeper.Client.ensure_path(self.ZooKeeper.Path)
+		self.ZooKeeper.Client.ensure_path(self.Path)
 
 		def in_main_thread():
 			self.App.PubSub.publish("ZooKeeperContainer.started!", self)
-
 		self.App.Loop.call_soon_threadsafe(in_main_thread)
 
 
@@ -85,21 +176,25 @@ class ZooKeeperContainer(ConfigObject):
 		return self.ZooKeeper.Client.connected
 
 
+	# Advertisement into Zookeeper
+
 	def advertise(self, data, path):
-		adv = self.Advertisments.get(self.ZooKeeper.Path + path)
+		adv = self.Advertisments.get(self.Path + path)
 		if adv is None:
-			adv = ZooKeeperAdvertisement(self.ZooKeeper.Path + path)
-			self.Advertisments[self.ZooKeeper.Path + path] = adv
-		adv.set_data(data)
-		self.App.TaskService.schedule(adv._do_advertise(self))
+			adv = ZooKeeperAdvertisement(self.Path + path)
+			self.Advertisments[self.Path + path] = adv
+		self.App.TaskService.schedule(adv._set_data(self, data))
 
 
-	async def _do_advertise(self, *args):
+	async def _readvertise(self, *args):
 		for adv in self.Advertisments.values():
-			await adv._do_advertise(self)
+			await adv._readvertise(self)
+
+
+	# Reading
 
 	async def get_children(self):
-		return await self.ZooKeeper.get_children(self.ZooKeeper.Path)
+		return await self.ZooKeeper.get_children(self.Path)
 
 	async def get_data(self, child, encoding="utf-8"):
 		raw_data = await self.get_raw_data(child)
@@ -108,7 +203,10 @@ class ZooKeeperContainer(ConfigObject):
 		return json.loads(raw_data.decode(encoding))
 
 	async def get_raw_data(self, child):
-		return await self.ZooKeeper.get_data("{}/{}".format(self.ZooKeeper.Path, child))
+		return await self.ZooKeeper.get_data("{}/{}".format(self.Path, child))
+
+
+	# Watcher
 
 	def _on_watcher_trigger(self, data, stat):
 		def on_watcher_trigger():
@@ -124,32 +222,50 @@ class ZooKeeperContainer(ConfigObject):
 class ZooKeeperAdvertisement(object):
 
 	def __init__(self, path):
-		self.Path = path
 		self.Data = None
-		self.Node = None
+		
+		self.Path = path
+		self.RealPath = None
+		
 		self.Lock = asyncio.Lock()
 
 
-	def set_data(self, data):
-		if isinstance(data, dict):
-			self.Data = json.dumps(data).encode("utf-8")
-		elif isinstance(data, str):
-			self.Data = data.encode("utf-8")
-		else:
-			self.Data = data
+	async def _set_data(self, zoocontainer, data):
+		async with self.Lock:
+			if isinstance(data, dict):
+				self.Data = json.dumps(data).encode("utf-8")
+			elif isinstance(data, str):
+				self.Data = data.encode("utf-8")
+			else:
+				self.Data = data
+
+			def write_to_zk():
+				if self.RealPath is None:
+					self.RealPath = zoocontainer.ZooKeeper.Client.create(self.Path, self.Data, sequence=True, ephemeral=True, makepath=True)
+				else:
+					try:
+						zoocontainer.ZooKeeper.Client.set(self.RealPath, self.Data)
+					except kazoo.exceptions.NoNodeError:
+						self.RealPath = zoocontainer.ZooKeeper.Client.create(self.Path, self.Data, sequence=True, ephemeral=True, makepath=True)
+
+			await zoocontainer.ZooKeeper.ProactorService.execute(write_to_zk)
 
 
-	async def _do_advertise(self, zoocontainer):
+	async def _readvertise(self, zoocontainer):
 		if self.Data is None:
 			return
 
-		async with self.Lock:
-			if self.Node is not None and await zoocontainer.ZooKeeper.exists(self.Node):
-				await zoocontainer.ZooKeeper.set_data(self.Node, self.Data)
-				return
+		if self.RealPath is None:
+			return
 
-			try:
-				self.Node = await zoocontainer.ZooKeeper.create(self.Path, self.Data, sequential=True, ephemeral=True)
-			except kazoo.exceptions.NoNodeError:
-				await zoocontainer.ZooKeeper.ensure_path(self.Path.rstrip(self.Path.split("/")[-1]))
-				self.Node = await zoocontainer.ZooKeeper.create(self.Path, self.Data, sequential=True, ephemeral=True)
+		async with self.Lock:
+
+			def check_at_zk():
+				if zoocontainer.ZooKeeper.Client.exists(self.RealPath):
+					return
+
+				# If the advertisement node is not present in the Zookeeper, force the recreation
+				self.RealPath = zoocontainer.ZooKeeper.Client.create(self.Path, self.Data, sequence=True, ephemeral=True)
+
+			await zoocontainer.ZooKeeper.ProactorService.execute(check_at_zk)
+
--- asab/zookeeper/wrapper.py
+++ asab/zookeeper/wrapper.py
@@ -1,5 +1,4 @@
 import logging
-import urllib.parse
 
 import kazoo.client
 import kazoo.exceptions
@@ -12,74 +11,11 @@ L = logging.getLogger(__name__.rsplit(".", 1)[0])  # We want just "asab.zookeepe
 
 
 class KazooWrapper(object):
-	"""
-	This function builds ZooKeeper clients from Configs and URLs
 
-	Supported types of URLs:
 
-	1. Absolute URL.
-	Example: zookeeper://zookeeper:12181/etc/configs/file1
-
-	2. Relative URL with full path
-		Example: zookeeper:///etc/configs/file1
-		In this case the relative url is expanded as follows:
-		zookeeper://{default_server}/etc/configs/file1
-		Where {default_server} is substituted with the server entry of the [zookeeper] configuration file section.
-
-	3. Relative URL with relative path
-	Example: zookeeper:./etc/configs/file1
-		In this case, the relative URL is expanded as follows:
-		zookeper://{default_server}/{default_path}/etc/configs/file1
-		Where {default_server} is substituted with the "server" entry of the [zookeeper] configuration file section and
-		{default_path} is substituted with the "path" entry of the [zookeeper] configuration file section.
-
-	Sample config file:
-	[asab.zookeeper]
-	server=server1:port1,server2:port2,server3:port3
-	path=myfolder
-	"""
-
-	def __init__(self, app, config, z_url):
-
-		# Make sure that the proactor service exists
-		from ..proactor import Module
-		app.add_module(Module)
-		self.ProactorService = app.get_service("asab.ProactorService")
-
-		# Parse URL
-		if z_url is not None:
-			url_pieces = urllib.parse.urlparse(z_url)
-			url_netloc = url_pieces.netloc
-			url_path = url_pieces.path
-		else:
-			url_netloc = ''
-			url_path = ''
-
-		# If there is no location, use the value of 'servers' from the configuration
-		if url_netloc == '':
-			url_netloc = config.get("servers")
-			if url_netloc == '' or url_netloc is None:
-				# if server entry is missing exit
-				L.critical("Cannot connect to Zookeeper, the configuration value of 'servers' not provided: '{}'".format(config))
-				raise SystemExit("Exit due to a critical configuration error.")
-
-		# If path has not been provided, use the value of 'path' from the configuration
-		if url_path == '':
-			url_path = config.get("path")
-
-		# If path still has not been found (not in configuration), derive the path from the application class name
-		if url_path == '' or url_path is None:
-			url_path = app.__class__.__name__
-
-		# Remove all heading '/' from path
-		while url_path.startswith('/'):
-			url_path = url_path[1:]
-
-		self.Path = url_path.rstrip('/')
-		self.Hosts = url_netloc
-
-		# Create and return the client and the url-path
-		self.Client = kazoo.client.KazooClient(hosts=self.Hosts)
+	def __init__(self, zksvc, hosts):
+		self.ProactorService = zksvc.ProactorService
+		self.Client = kazoo.client.KazooClient(hosts=hosts)
 
 
 	# connection start/close calls
@@ -150,9 +86,20 @@ class KazooWrapper(object):
 			return None
 		return ret
 
-	# create ephemeral node
-	async def create(self, path, value, sequential, ephemeral):
+	async def create(self, path, value, sequence=False, ephemeral=False, makepath=False):
 
-		ret = await self.ProactorService.execute(
-			self.Client.create, path, value, None, ephemeral, sequential, False)
-		return ret
+		def do():
+			return self.Client.create(path, value=value, ephemeral=ephemeral, sequence=sequence, makepath=makepath)
+
+		return await self.ProactorService.execute(do)
+
+
+	# create a new node or update the existing one
+	async def upsert(self, path, value):
+		def do():
+			try:
+				return self.Client.create(path, value=value)
+			except kazoo.exceptions.NodeExistsError:
+				return self.Client.set(path, data)
+
+		return await self.ProactorService.execute(do)
