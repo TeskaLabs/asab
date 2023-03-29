15c70546f67879b3319154e94b030f6e691dcc2b
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2023-03-29 10:44:19 +0200

    Extract the string to boolean conversion used in config into utils method.

--- asab/utils.py
+++ asab/utils.py
@@ -1,4 +1,5 @@
 import urllib.parse
+import configparser
 
 
 def convert_to_seconds(value: str) -> float:
@@ -31,6 +32,17 @@ def convert_to_seconds(value: str) -> float:
 		raise ValueError("'{}' is not a valid time specification: {}.".format(value, e))
 
 	return value
+
+
+def string_to_boolean(value: str) -> bool:
+	"""
+	Convert common boolean string values (e.g. "yes" or "no") into boolean.
+	"""
+	if isinstance(value, bool):
+		return value
+	if value.lower() not in configparser.ConfigParser.BOOLEAN_STATES:
+		raise ValueError("Not a boolean: {}".format(value))
+	return configparser.ConfigParser.BOOLEAN_STATES[value.lower()]
 
 
 def validate_url(input_url: str, scheme):
