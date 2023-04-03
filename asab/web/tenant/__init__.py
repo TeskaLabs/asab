3d79e37b883187926e5afddc8302ec508f82bb43
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2023-04-03 19:25:59 +0200

    Delete old tenant module

--- asab/web/tenant/__init__.py
+++
@@ -1,9 +0,0 @@
-from .service import TenantService
-from .midleware import tenant_middleware_factory, tenant_handler
-
-
-__all__ = (
-	'TenantService',
-	'tenant_middleware_factory',
-	'tenant_handler',
-)
