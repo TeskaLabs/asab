a4098fe718840462f672572054c2d9db3fae9b19
eliska <eliska.novotna@protonmail.com>
2023-04-04 18:49:35 +0200

    draft of discovery custom session

--- asab/api/discovery.py
+++ asab/api/discovery.py
@@ -1,6 +1,12 @@
 import logging
 import json
 
+import aiohttp
+
+from typing import List, Dict, Any
+import socket
+import asyncio
+
 from .. import Service
 
 
@@ -51,6 +57,43 @@ class DiscoveryService(Service):
 				urls.append("http://{}:{}".format(servername, port))
 		return urls
 
+	async def _locate(self, instance_id: str = None, service_id: str = None, appclass: str = None) -> list:
+		if instance_id is None and appclass is None and service_id is None:
+			L.warning("Please provide instance_id, service_id, or appclass to locate the service(s).")
+			return
+
+		instances = await self.get_advertised_instances()
+		if len(instances) == 0:
+			L.warning("No instances available.")
+			return
+		res = []
+		for instance in instances:
+			if appclass is not None:
+				if appclass != instance.get("appclass", "").lower():
+					continue
+
+			if service_id is not None:
+				if service_id != instance.get("service_id", "").lower():
+					continue
+
+			if instance_id is not None:
+				if instance_id != instance.get("instance_id", "").lower():
+					continue
+
+			web = instance.get("web")
+			servername = instance.get("servername")
+			if web is None or servername is None:
+				continue
+			for i in web:
+				try:
+					# ip = i[0]
+					port = i[1]
+				except KeyError:
+					L.error("Unexpected format of 'web' section in advertised data: '{}'".format(web))
+					return
+				res.append((servername, port))
+		return res
+
 
 	async def get_advertised_instances(self):
 		advertised = []
@@ -72,3 +115,24 @@ class DiscoveryService(Service):
 		for item in items:
 			item_data = await self.ZooKeeperContainer.ZooKeeper.get_data(base_path + '/' + item)
 			yield item, item_data
+
+	async def session(self):
+		return aiohttp.ClientSession(connector=aiohttp.TCPConnector(resolver=DiscoveryResolver(self)))
+
+
+class DiscoveryResolver(aiohttp.DefaultResolver):
+	"""Custom aiohttpResolver for Discovery Session"""
+
+	def __init__(self, svc) -> None:
+		self._loop = asyncio.get_running_loop()
+		self.DiscoveryService = svc
+
+
+	async def resolve(self, hostname: str, port: int = 0, family: int = socket.AF_INET) -> List[Dict[str, Any]]:
+		value, key = hostname.split(".")
+		listik = await self.DiscoveryService._locate(**{key: value})
+		hosts = []
+		for i in listik:
+			hosts.append(*await super().resolve(i[0], i[1], family))
+
+		return hosts
--- examples/discovery.py
+++ examples/discovery.py
@@ -1,5 +1,6 @@
 import logging
 import aiohttp
+import aiohttp.web
 
 import asab
 import asab.web
@@ -50,24 +51,20 @@ class MyApplication(asab.Application):
 		self.WebContainer.WebApp.router.add_get('/locate', self.locate_self)
 
 	async def locate_self(self, request):
-		# This `locate()` method seeks for its application in the ZooKeeper. Thus, it calls itself, being a tiny Oroboros.
+		# This method seeks for MyApplication in the ZooKeeper. Thus, it calls itself, being a tiny Oroboros.
 		# Try to run more than one ASAB Application with the same ZooKeeper configuration.
 		# You will get a tool to locate any service in the "cluster".
 
-		urls = await self.DiscoveryService.locate(appclass="MyApplication")
-		if urls is None:
-			print("Application was not advertised properly")
-
 		# Get config of the application:
 		config = None
-		async with aiohttp.ClientSession() as session:
-			while len(urls) > 0:  # if more than one url is provided, try them all until one of them returns 200
-				url = "{}/asab/v1/config".format(urls.pop())
-				print("trying to connect to: {}".format(url))
-				async with session.get(url) as resp:
-					if resp.status == 200:
-						config = await resp.json()
-						break
+		session = await self.DiscoveryService.session()
+		try:
+			async with session.get("http://MyApplication.appclass/asab/v1/config") as resp:
+				if resp.status == 200:
+					config = await resp.json()
+
+		finally:
+			await session.close()
 
 		if config is None:
 			return aiohttp.web.json_response({"result": "FAILED"})
