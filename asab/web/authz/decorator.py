2003dfc6584853ef970cc60143d405fea24236dc
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2023-04-18 16:48:38 +0200

    Add require_superuser decorator

--- asab/web/authz/decorator.py
+++ asab/web/authz/decorator.py
@@ -39,6 +39,30 @@ def require(*resources):
 	return decorator_required
 
 
+def require_superuser(handler):
+	"""
+	Check if the request has superuser access. Failure results in HTTP 403 response.
+
+	Usage:
+	```python3
+	@asab.web.authz.require("my-app:token:generate")
+	async def generate_token(self, request):
+		data = await self.service.generate_token()
+		return asab.web.rest.json_response(request, data)
+	```
+	"""
+	@functools.wraps(handler)
+	async def wrapper(*args, **kwargs):
+		request = args[-1]
+
+		if not request.has_superuser_access():
+			raise aiohttp.web.HTTPForbidden()
+
+		return await handler(*args, **kwargs)
+
+	return wrapper
+
+
 def no_auth(handler):
 	"""
 	Skip request authentication and authorization for the decorated handler.
