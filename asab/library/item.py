a0e6640cc4c5027a83a93390bcdc6c9084c3fdfd
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-05-09 07:07:36 +0200

    add docdtring

--- asab/library/item.py
+++ asab/library/item.py
@@ -4,21 +4,25 @@ import dataclasses
 @dataclasses.dataclass
 class LibraryItem:
     '''
-    The data class that contains the info about specific item in the library.
+    The data class that contains the info about a specific item in the library.
 
-    * `name` is full name and path of the item.
-    It MUST start with `/` and contains the whole absolute path.
-    The `name` can be directly fed into `LibraryService.read(...)`.
+    * `name` is the full name and path of the item.
+      It MUST start with `/` and contain the whole absolute path.
+      The `name` can be directly fed into `LibraryService.read(...)`.
 
-    * `type` is `item` or `dir`
+    * `type` is `item` or `dir`.
+
+    * `layer` is an integer indicating the level of the provider that provides this item.
+      A higher number indicates a higher level of the provider in the library hierarchy.
 
     * `providers` is a list of providers that provide this item.
-    `dir` LibaryItems can be provided by more than one provider.
+      `dir` LibraryItems can be provided by more than one provider.
 
-    * `disabled` if True, then this item is disabled. `LibraryService.read(...)` will return `None`.
+    * `disabled` if True, then this item is disabled and `LibraryService.read(...)` will return `None`.
 
-    * `override` if True, then this item is marked as override for the providers with the same item name.
+    * `override` if True, then this item is marked as an override for the providers with the same item name.
     '''
+
 
     name: str
     type: str
