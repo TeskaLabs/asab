aa2d03c3d8d24c73023df06fe03e022b4a5f9cb6
mejroslav <miroslav.burysek@gmail.com>
2023-07-21 16:03:46 +0200

    Extend 'Timer' docstrings

--- asab/timer.py
+++ asab/timer.py
@@ -1,27 +1,38 @@
 import asyncio
+import typing
 
 
 class Timer(object):
-	'''
-	T.__init__(app, handler, autorestart=False) -> Timer.
-
+	"""
 	The relative and optionally repeating timer for asyncio.
 
-	This class is simple relative timer that generate an event after a given time, and optionally repeating in regular intervals after that.
-
-	:param app: An ASAB application.
-	:param handler: A coro or future that will be called when a timer triggers.
-	:param boolean autorestart: If `True` then a timer will be automatically restarted after triggering.
-
-	:ivar Handler: A coro or future that will be called when a timer triggers.
-	:ivar Task: A future that represent the timer task.
-	:ivar App: An ASAB app.
-	:ivar boolean AutoRestart: If `True` then a timer will be automatically restarted after triggering.
-
+	This class is simple relative timer that generate an event after a given time,
+	and optionally repeating in regular intervals after that.
 	The timer object is initialized as stopped.
 
-	*Note*: The implementation idea is borrowed from "`Python - Timer with asyncio/coroutine <https://stackoverflow.com/questions/45419723/python-timer-with-asyncio-coroutine>`_" question on StackOverflow.
-	'''
+	Attributes:
+		App (asab.Application): Reference to the ASAB application.
+		Handler (asyncio.Coroutine | asyncio.Task): A coroutine or future that will be called when a timer triggers.
+		Task (asyncio.Future | asyncio.Task | None): A future that represents the timer task.
+		AutoRestart (bool): If `True` then a timer will be automatically restarted after triggering.
+
+	Examples:
+	```python
+	class TimerApplication(asab.Application):
+		async def initialize(self):
+			self.Timer = asab.Timer(self, self.on_tick, autorestart=True)
+			self.Timer.start(1) #(1)
+
+		async def on_tick(self): #(2)
+			print("Think!")
+	```
+
+	1. The timer will trigger a message publishing at every second.
+	2. This function has to be a coroutine.
+
+	!!! note
+		The implementation idea was borrowed from [StackOverflow discussion](https://stackoverflow.com/questions/45419723/python-timer-with-asyncio-coroutine).
+	"""
 
 	def __init__(self, app, handler, autorestart=False):
 		self.App = app
@@ -32,43 +43,50 @@ class Timer(object):
 		app.PubSub.subscribe("Application.stop!", self._on_stop)
 
 
-	def start(self, timeout):
-		'''
-		Start the timer.
+	def start(self, timeout: typing.Union[int, float]):
+		"""
+		Start the `Timer` with new timeout.
 
-		:param float/int timeout: A timer delay in seconds.
-		'''
+		Args:
+			timeout (int | float): A timer delay in seconds.
+
+		Raises:
+			RuntimeError: If the `Timer` has already started.
+		"""
 		if self.is_started():
 			raise RuntimeError("Timer is already started")
 		self.Task = asyncio.ensure_future(self._job(timeout))
 
 
 	def stop(self):
-		'''
+		"""
 		Stop the timer.
-		'''
+		"""
 		if self.Task is not None:
 			self.Task.cancel()
 			self.Task = None
 
 
-	def restart(self, timeout):
-		'''
-		Restart the timer.
+	def restart(self, timeout: typing.Union[int, float]):
+		"""
+		Restart the timer with a new timeout.
 
-		:param float/int timeout: A timer delay in seconds.
-		'''
+		Args:
+			timeout (int | float): A new timeout in seconds.
+		"""
 
 		if self.is_started():
 			self.stop()
 		self.start(timeout)
 
 
-	def is_started(self):
-		'''
-		T.is_started() -> boolean
-		Return `True` is the timer is started otherwise returns `False`.
-		'''
+	def is_started(self) -> bool:
+		"""
+		Check if the `Timer` has started.
+
+		Returns:
+			`True` if the `Timer` has started.
+		"""
 		return self.Task is not None
 
 
