8657a31f3d6afa0cc28b0bfd06399cf268c009d2
mejroslav <miroslav.burysek@gmail.com>
2023-08-02 22:04:51 +0200

    Add docstrings to WebService

--- asab/web/service.py
+++ asab/web/service.py
@@ -11,6 +11,25 @@ L = logging.getLogger(__name__)
 
 
 class WebService(Service):
+	"""
+	Service for running and easy manipulation of the web server.
+	It is used for registering and running the web container as well as initialization of web request metrics.
+
+	It should be used together with `asab.web.WebContainer` object that handles the web configuration.
+
+	Examples:
+
+	```python
+	from asab.web import Module
+	self.add_module(Module)
+	web_service = self.get_service("asab.WebService")
+	container = asab.web.WebContainer(
+		websvc=web_service,
+		config_section_name='my:web',
+		config={"listen": "0.0.0.0:8080"}
+	)
+	```
+	"""
 
 	ConfigSectionAliases = ["asab:web"]
 
@@ -35,17 +54,17 @@ class WebService(Service):
 
 	@property
 	def WebApp(self):
-		'''
-		This is here to maintain backward compatibility.
-		'''
+		"""
+		An obsolete property only for maintaining backward compatibility. Please use `asab.web.WebContainer.WebApp` instead.
+		"""
 		return self.WebContainer.WebApp
 
 
 	@property
 	def WebContainer(self):
-		'''
-		This is here to maintain backward compatibility.
-		'''
+		"""
+		An obsolete property only for maintaining backward compatibility. Please use `asab.web.WebContainer` instead.
+		"""
 		config_section = "web"
 
 		# The WebContainer should be configured in the config section [web]
