1016644c28c866b9509baaefcc7bfa36508d07e9
mejroslav <miroslav.burysek@gmail.com>
2023-08-04 16:41:41 +0200

    Fix indentation for mkdocstring

--- asab/web/__init__.py
+++ asab/web/__init__.py
@@ -21,30 +21,30 @@ class Module(Module):
 
 def create_web_server(app, section: str = "web", config: typing.Optional[dict] = None) -> aiohttp.web.UrlDispatcher:
 	"""
-	Build the web server with the specified configuration.
+Build the web server with the specified configuration.
 
-	It is an user convenience function that simplifies typical way of how the web server is created.
+It is an user convenience function that simplifies typical way of how the web server is created.
 
-	Args:
-		app (asab.Application): A reference to the ASAB Application.
-		section (str): Configuration section name with which the WebContainer will be created.
-		config (dict | None): Additional server configuration.
+Args:
+	app (asab.Application): A reference to the ASAB Application.
+	section (str): Configuration section name with which the WebContainer will be created.
+	config (dict | None): Additional server configuration.
 
-	Returns:
-		[WebContainer Application Router object](https://docs.aiohttp.org/en/stable/web_reference.html?highlight=router#router).
+Returns:
+	[WebContainer Application Router object](https://docs.aiohttp.org/en/stable/web_reference.html?highlight=router#router).
 
-	Examples:
+Examples:
 
-	```python
-	class MyApplication(asab.Application):
-		async def initialize(self):
-			web = asab.web.create_web_server(self)
-			web.add_get('/hello', self.hello)
+```python
+class MyApplication(asab.Application):
+	async def initialize(self):
+		web = asab.web.create_web_server(self)
+		web.add_get('/hello', self.hello)
 
-		async def hello(self, request):
-			return asab.web.rest.json_response(request, data="Hello, world!\n")
+	async def hello(self, request):
+		return asab.web.rest.json_response(request, data="Hello, world!\n")
 
-	```
+```
 	"""
 	app.add_module(Module)
 	websvc = app.get_service("asab.WebService")
