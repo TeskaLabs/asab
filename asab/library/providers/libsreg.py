3c7b15cee5c8c5754fd243532d2a3748cc63b2d9
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-10-16 16:00:29 +0200

    check to delete

--- asab/library/providers/libsreg.py
+++ asab/library/providers/libsreg.py
@@ -21,7 +21,6 @@ L = logging.getLogger(__name__)
 Config.add_defaults(
 	{
 		'library': {
-			# You may specify multiple ElasticSearch nodes by e.g. http://es01:9200,es02:9200,es03:9200/
 			'repo_sub_path': '/'
 		}
 	}
@@ -29,32 +28,13 @@ Config.add_defaults(
 
 
 class LibsRegLibraryProvider(FileSystemLibraryProvider):
-	"""
-	Read-only provider to read from remote "library repository".
-
-	It provides an option to specify more servers for more reliable content delivery.
-
-	Example of the configuration:
-
-	```ini
-	[library]
-	providers=
-	...
-	libsreg+https://libsreg.z6.web.core.windows.net,libsreg-secondary.z6.web.core.windows.net/lmio-common-library
-	...
-	```
-
-	"""
 
 	def __init__(self, library, path, layer):
 
 		url = urllib.parse.urlparse(path)
 		assert url.scheme.startswith('libsreg+')
 
-		version = url.fragment
-		if version == '':
-			version = 'master'
-
+		version = url.fragment if url.fragment else 'master'
 		archname = url.path[1:]
 
 		self.URLs = ["{scheme}://{netloc}/{archname}/{archname}-{version}.tar.xz".format(
@@ -66,38 +46,31 @@ class LibsRegLibraryProvider(FileSystemLibraryProvider):
 		assert len(self.URLs) > 0
 
 		tempdir = tempfile.gettempdir()
-		self.RepoSubPath = Config.get('library', 'repo_sub_path')
 		self.RepoPath = os.path.join(
 			tempdir,
 			"asab.library.libsreg",
 			hashlib.sha256(self.URLs[0].encode('utf-8')).hexdigest()
 		)
 
-		if self.RepoSubPath == "/":
-			# No additional subdirectory, use the base directory as is.
-			self.FinalPath = self.RepoPath
-		else:
-			# Append the subdirectory to the base path and create it.
-			self.FinalPath = os.path.join(self.RepoPath, self.RepoSubPath)
+		# Ensure the base repository path exists
+		os.makedirs(self.RepoPath, exist_ok=True)
 
-		# Ensure the directory exists
+		# Determine the final path based on the configuration
+		repo_sub_path = Config.get('library', 'repo_sub_path')
+		self.FinalPath = os.path.join(self.RepoPath, repo_sub_path.strip("/")) if repo_sub_path != "/" else self.RepoPath
+
+		# Ensure the final path exists
 		os.makedirs(self.FinalPath, exist_ok=True)
 
 		super().__init__(library, self.RepoPath, layer, set_ready=False)
 
 		self.PullLock = False
-
-		# TODO: Subscription to changes in the library
 		self.SubscribedPaths = set()
 
 		self.App.TaskService.schedule(self._periodic_pull(None))
 		self.App.PubSub.subscribe("Application.tick/60!", self._periodic_pull)
 
-
 	async def _periodic_pull(self, event_name):
-		"""
-		Changes in remote repository are being pulled every minute. `PullLock` flag ensures that only if previous "pull" has finished, new one can start.
-		"""
 		if self.PullLock:
 			return
 
@@ -105,8 +78,6 @@ class LibsRegLibraryProvider(FileSystemLibraryProvider):
 
 		try:
 			headers = {}
-
-			# Check for existing E-Tag
 			etag_fname = os.path.join(self.RepoPath, "etag")
 			try:
 				with open(etag_fname, 'r') as f:
@@ -120,10 +91,14 @@ class LibsRegLibraryProvider(FileSystemLibraryProvider):
 			async with aiohttp.ClientSession() as session:
 				async with session.get(url, headers=headers) as response:
 
-					if response.status == 200:  # The request indicates a new version that we don't have yet
-
+					if response.status == 200:
 						etag_incoming = response.headers.get('ETag')
-						fname = os.path.join(self.FinalPath, "new.tar.xz")
+
+						# Ensure the new directory exists before writing the file
+						new_dir = os.path.join(self.RepoPath, "new")
+						os.makedirs(new_dir, exist_ok=True)
+
+						fname = os.path.join(new_dir, "new.tar.xz")
 						with open(fname, 'wb') as ftmp:
 							while True:
 								chunk = await response.content.read(16 * 1024)
@@ -131,36 +106,31 @@ class LibsRegLibraryProvider(FileSystemLibraryProvider):
 									break
 								ftmp.write(chunk)
 
-						# TODO: Following code is potentionally blocking and should be done in a proactor
-						# ⬇️⬇️⬇️ ---------- START OF THE BLOCKING CODE
-
 						with tarfile.open(fname, mode='r:xz') as tar:
-							tar.extractall(os.path.join(self.FinalPath, "new"))
+							tar.extractall(new_dir)
 
 						os.unlink(fname)
 
-						# Move the new content in place
-						synchronize_dirs(self.FinalPath, os.path.join(self.RepoPath, "new"))
-						path_to_remove = os.path.join(self.RepoPath, "new")
-						if os.path.exists(path_to_remove):
-							shutil.rmtree(path_to_remove)
+						# Synchronize the directories
+						synchronize_dirs(self.FinalPath, new_dir)
+						# Safely remove the new directory if it exists
+						if os.path.exists(new_dir):
+							shutil.rmtree(new_dir)
+						else:
+							L.warning("Directory not found for removal: {}".format(new_dir))
 
-						if etag_incoming is not None:
+					if etag_incoming is not None:
 							with open(etag_fname, 'w') as f:
 								f.write(etag_incoming)
 
-						# ⬆️⬆️⬆️ --------- END OF THE BLOCKING CODE
-
 					elif response.status == 304:
-						# The repository has not changed ...
-						pass
+						pass  # No changes
 
 					else:
 						L.exception("Failed to download the library.", struct_data={"url": url, 'status': response.status})
 
 		finally:
 			self.PullLock = False
-
 
 	async def subscribe(self, path):
 		self.SubscribedPaths.add(path)
