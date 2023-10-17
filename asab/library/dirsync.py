7d5f646c525016e2b24f8f33f3838d0b1e145404
mshivashankar <mithun.shivashankar@teskalabs.com>
2023-10-17 05:49:46 +0200

    sync update

--- asab/library/dirsync.py
+++ asab/library/dirsync.py
@@ -35,3 +35,34 @@ def synchronize_dirs(target, source):
 			# Copy if the file doesn't exist in target or if it's modified
 			if not os.path.exists(target_file) or not filecmp.cmp(source_file, target_file, shallow=False):
 				shutil.copy2(source_file, target_file)
+
+	# Step 2: Recursively delete files/folders in target that don't exist in source
+	for dirpath, dirnames, filenames in os.walk(target, topdown=False):  # topdown=False for depth-first
+		relpath = os.path.relpath(dirpath, target)
+		source_dir = os.path.join(source, relpath)
+
+		# Skip the 'new' directory in the target
+		if os.path.normpath(dirpath) == os.path.normpath(os.path.join(target, 'new')):
+			continue
+
+		# Check and remove files not in source
+		for filename in filenames:
+			target_file = os.path.join(dirpath, filename)
+			source_file = os.path.join(source_dir, filename)
+
+			# Do not remove if the file is in the 'new' directory
+			if 'new' not in os.path.relpath(target_file, target).split(os.path.sep):
+				if not os.path.exists(source_file):
+					os.remove(target_file)
+
+		# Check and remove directories not in source
+		for dirname in dirnames:
+			target_subdir = os.path.join(dirpath, dirname)
+			source_subdir = os.path.join(source_dir, dirname)
+
+			# Do not remove the 'new' directory or its subdirectories
+			if 'new' in os.path.relpath(target_subdir, target).split(os.path.sep):
+				continue
+
+			if not os.path.exists(source_subdir):
+				shutil.rmtree(target_subdir)
