877e17201129b7cefa7e10e8eb986debfeac0dea
Elena Tataush <tataushelena@gmail.com>
2023-11-21 17:00:14 +0100

    Going back to old init; only asab:storage initiates StorageService

--- asab/storage/__init__.py
+++ asab/storage/__init__.py
@@ -12,7 +12,6 @@ class Module(asab.Module):
 
 	def __init__(self, app):
 		super().__init__(app)
-		# old configuration format [asab:storage]
 		sttype = asab.Config.get('asab:storage', 'type', fallback=None)
 
 		if sttype == 'inmemory':
@@ -27,12 +26,8 @@ class Module(asab.Module):
 			from .elasticsearch import StorageService
 			self.Service = StorageService(app, "asab.StorageService")
 
-		# new configuration format
 		elif sttype is None:
-			# [elasticsearch]
-			if asab.Config.has_section('elasticsearch'):
-				from .elasticsearch import StorageService
-				self.Service = StorageService(app, "asab.StorageService", config_section_name='elasticsearch')
+			L.warning("Storage type is not specified in the configuration.")
 
 		else:
-			L.error("Unknown asab:storage type '{}' or unsupported configuration format".format(sttype))
+			L.error("Unknown asab:storage type '{}'".format(sttype))
