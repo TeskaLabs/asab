828b6e73b75bf3c75dfaaad48a46111760b71b63
mshivashankar <mithun.shivashankar@teskalabs.com>
2024-02-13 07:48:42 +0100

    Flake8

--- asab/library/providers/zookeeper.py
+++ asab/library/providers/zookeeper.py
@@ -309,8 +309,7 @@ class ZooKeeperLibraryProvider(LibraryProviderABC):
 			node_path += "/.tenants/" + tenant
 
 		assert '//' not in node_path, "Directory path cannot contain double slashes (//). Example format: /library/Templates/"
-		assert node_path[
-				   0] == '/', "Directory path must start with a forward slash (/). For example: /library/Templates/"
+		assert node_path[0] == '/', "Directory path must start with a forward slash (/). For example: /library/Templates/"
 
 		return node_path
 
