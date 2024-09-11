b84c81fd47a431c8d6d64bdc98b1cbe5c7201b37
mburysek <miroslav.burysek@teskalabs.com>
2024-09-11 11:40:24 +0200

    Disable transaction sampling

--- asab/sentry/__init__.py
+++ asab/sentry/__init__.py
@@ -6,7 +6,7 @@ Config.add_defaults(
 		"sentry": {
 			"data_source_name": "",
 			"environment": "not specified",
-			"traces_sample_rate": 1.0,
+			"traces_sample_rate": 0,  # https://docs.sentry.io/platforms/python/configuration/sampling/#configuring-the-transaction-sample-rate
 		},
 
 		"sentry:logging": {
