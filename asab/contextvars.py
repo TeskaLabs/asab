e298f6483b571776540ea6a4449e3b08fbbbbffd
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2024-10-07 14:58:50 +0200

    add comments with types

--- asab/contextvars.py
+++ asab/contextvars.py
@@ -1,4 +1,7 @@
 import contextvars
 
+# Contains tenant ID string
 Tenant = contextvars.ContextVar("Tenant")
+
+# Contains asab.web.auth.Authorization object
 Authz = contextvars.ContextVar("Authz")
