4c23e3bb9d3045e942045328a89c4c8ba29f3674
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2024-10-07 14:59:41 +0200

    add docstrings

--- asab/web/auth/authz.py
+++ asab/web/auth/authz.py
@@ -1,4 +1,9 @@
 import typing
+import logging
+
+from asab.exceptions import AccessDeniedError
+
+L = logging.getLogger(__name__)
 
 
 SUPERUSER_RES_ID = "authz:superuser"
@@ -10,7 +15,12 @@ class Authorization:
 	"""
 	def __init__(self, auth_service, userinfo: dict, tenant: typing.Union[str, None]):
 		self.AuthService = auth_service
-		self.UserInfo = userinfo
+		if self.AuthService.is_enabled() and userinfo is None:
+			L.error("Userinfo is mandatory when AuthService is enabled.")
+			raise AccessDeniedError()
+		self.UserInfo = userinfo or {}
+		self.Tenant = tenant
+
 		self.CredentialsId = userinfo.get("sub")
 		self.Username = userinfo.get("preferred_username") or userinfo.get("username")
 		self.Email = userinfo.get("email")
@@ -18,8 +28,6 @@ class Authorization:
 
 		self.Issuer = self.UserInfo.get("iss")  # Who issued the authorization
 		self.AuthorizedParty = self.UserInfo.get("azp")  # What party (application) is authorized
-
-		self.Tenant = tenant
 
 	def __repr__(self):
 		return "<Authorization [{}cid: {!r}, azp: {!r}]>".format(
@@ -29,11 +37,22 @@ class Authorization:
 		)
 
 	def is_superuser(self) -> bool:
+		"""
+		Check whether the agent is a superuser.
+
+		:return: Is the agent a superuser?
+		"""
 		if not self.AuthService.is_enabled():
 			return True
 		return is_superuser(self.UserInfo)
 
-	def has_resource_access(self, resource_id: str | typing.Iterable[str]) -> bool:
+	def has_resource_access(self, resource_id: typing.Union[str, typing.Iterable[str]]) -> bool:
+		"""
+		Check whether the agent is authorized to access a resource or multiple resources.
+
+		:param resource_id: Resource ID or a list of resource IDs whose authorization is required.
+		:return: Is resource access authorized?
+		"""
 		if not self.AuthService.is_enabled():
 			return True
 		if isinstance(resource_id, str):
@@ -42,6 +61,14 @@ class Authorization:
 			return has_resource_access(self.UserInfo, resource_id, tenant=self.Tenant)
 
 	def authorized_resources(self) -> typing.Set[str]:
+		"""
+		Return the set of EXPLICITLY authorized resources. (Use carefully with superusers.)
+
+		NOTE: If possible, use methods has_resource_access(resource_id) and is_superuser() instead of inspecting
+		the set of resources.
+
+		:return: Set of authorized resources.
+		"""
 		return get_authorized_resources(self.UserInfo, self.Tenant)
 
 
@@ -73,8 +100,8 @@ def has_resource_access(
 
 def has_tenant_access(user_info: typing.Mapping, tenant: str) -> bool:
 	"""
-	Check if the request is authorized to access a tenant.
-	If the request has superuser access, tenant access is always implicitly granted.
+	Check the agent's userinfo to see if they are authorized to access a tenant.
+	If the agent has superuser access, tenant access is always implicitly granted.
 	"""
 	if tenant == "*":
 		raise ValueError("Invalid tenant name: '*'")
@@ -88,8 +115,9 @@ def has_tenant_access(user_info: typing.Mapping, tenant: str) -> bool:
 def get_authorized_resources(user_info: typing.Mapping, tenant: typing.Union[str, None]) -> typing.Set[str]:
 	"""
 	Extract resources authorized within given tenant (or globally, if tenant is None).
+
 	:param user_info:
 	:param tenant:
-	:return:
+	:return: Set of authorized resources.
 	"""
 	return set(user_info.get("resources", {}).get(tenant if tenant is not None else "*", []))
