fa872c324dfc4d3f744dfee0df3cbdb24b8c085c
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2024-11-08 17:31:22 +0100

    initialize tenants in taskservice

--- asab/web/tenant/providers/web.py
+++ asab/web/tenant/providers/web.py
@@ -10,17 +10,21 @@ L = logging.getLogger(__name__)
 class WebTenantProvider(TenantProviderABC):
 	def __init__(self, app, config):
 		super().__init__(app, config)
+		self.TaskService = self.App.get_service("asab.TaskService")
 		self.TenantUrl = self.Config.get("tenant_url")
 
-		self.App.PubSub.subscribe("Application.tick/300!", self._update_tenants)
+		self.App.PubSub.subscribe("Application.tick/300!", self._every_five_minutes)
 
 
 	async def initialize(self, app):
-		await self._update_tenants()
+		self.TaskService.schedule(self._update_tenants())
+
+
+	async def _every_five_minutes(self, message_type=None):
+		self.TaskService.schedule(self._update_tenants())
 
 
 	async def _update_tenants(self, message_type=None):
-		# TODO: Proactor service
 		async with aiohttp.ClientSession() as session:
 			async with session.get(self.TenantUrl) as resp:
 				if resp.status == 200:
