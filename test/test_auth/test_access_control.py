773752b47cc2f517c9b8f0ee8bb802b74fafdcbc
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2024-12-06 12:11:28 +0100

    add test messages

--- test/test_auth/test_access_control.py
+++ test/test_auth/test_access_control.py
@@ -12,7 +12,7 @@ from .constants import (
 )
 
 
-class TestAccessControlCommonUser(unittest.TestCase):
+class TestCommonUser(unittest.TestCase):
 	Claims = {
 		"resources": {
 			"*": [
@@ -25,50 +25,53 @@ class TestAccessControlCommonUser(unittest.TestCase):
 		},
 	}
 
+
 	def test_tenant_resource_access(self):
 		self.assertFalse(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2, RESOURCE_3],
 				tenant=TENANT_1,
-			)
+			),
+			"RESOURCE_3 is not authorized in TENANT_1.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2],
 				tenant=TENANT_1,
-			)
+			),
+			"Both resources are authorized in TENANT_1.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant=TENANT_1,
-			)
+			),
+			"Resource is authorized in TENANT_1.",
 		)
+
 		self.assertFalse(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2],
 				tenant=TENANT_2,
-			)
+			),
+			"TENANT_2 is not authorized.",
 		)
+
 		self.assertFalse(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant=TENANT_2,
-			)
+			),
+			"TENANT_2 is not authorized.",
 		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_resource_access(
-				claims=self.Claims,
-				resources=[RESOURCE_1],
-				tenant="*",
-			)
-		)
+
 
 	def test_global_resource_access(self):
 		self.assertFalse(
@@ -76,61 +79,75 @@ class TestAccessControlCommonUser(unittest.TestCase):
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2],
 				tenant=None,
-			)
+			),
+			"RESOURCE_2 is not authorized globally.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant=None,
-			)
+			),
+			"RESOURCE_1 is authorized globally.",
 		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_resource_access(
+
+		with self.assertRaises(ValueError):
+			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant="*",
 			)
-		)
+
 
 	def test_tenant_access(self):
 		self.assertTrue(
 			has_tenant_access(
 				claims=self.Claims,
 				tenant=TENANT_1,
-			)
+			),
+			"TENANT_1 is authorized.",
 		)
+
 		self.assertFalse(
 			has_tenant_access(
 				claims=self.Claims,
 				tenant=TENANT_2,
-			)
+			),
+			"TENANT_2 is not authorized.",
 		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_tenant_access(
+
+		with self.assertRaises(ValueError):
+			has_tenant_access(
 				claims=self.Claims,
 				tenant="*",
 			)
-		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_tenant_access(
+
+		with self.assertRaises(ValueError):
+			has_tenant_access(
 				claims=self.Claims,
 				tenant=None,
 			)
-		)
+
 
 	def test_superuser(self):
 		self.assertFalse(
 			is_superuser(
 				claims=self.Claims,
-			)
+			),
+			"Superuser resource is not authorized.",
 		)
 
 
-class TestAccessControlSuperuser(unittest.TestCase):
+class TestTenantWithoutResources(unittest.TestCase):
+	Claims = {
+		"resources": {
+			TENANT_1: [],
+		},
+	}
+
+
+class TestSuperuser(unittest.TestCase):
 	Claims = {
 		"resources": {
 			"*": [
@@ -139,50 +156,53 @@ class TestAccessControlSuperuser(unittest.TestCase):
 		},
 	}
 
+
 	def test_tenant_resource_access(self):
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2, RESOURCE_3],
 				tenant=TENANT_1,
-			)
+			),
+			"Superuser must have unrestricted resource access.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2],
 				tenant=TENANT_1,
-			)
+			),
+			"Superuser must have unrestricted resource access.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant=TENANT_1,
-			)
+			),
+			"Superuser must have unrestricted resource access.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2],
 				tenant=TENANT_2,
-			)
+			),
+			"Superuser must have unrestricted resource access.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant=TENANT_2,
-			)
+			),
+			"Superuser must have unrestricted resource access.",
 		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_resource_access(
-				claims=self.Claims,
-				resources=[RESOURCE_1],
-				tenant="*",
-			)
-		)
+
 
 	def test_global_resource_access(self):
 		self.assertTrue(
@@ -190,55 +210,53 @@ class TestAccessControlSuperuser(unittest.TestCase):
 				claims=self.Claims,
 				resources=[RESOURCE_1, RESOURCE_2],
 				tenant=None,
-			)
+			),
+			"Superuser must have unrestricted resource access.",
 		)
+
 		self.assertTrue(
 			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant=None,
-			)
+			),
+			"Superuser must have unrestricted resource access.",
 		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_resource_access(
+
+		with self.assertRaises(ValueError):
+			has_resource_access(
 				claims=self.Claims,
 				resources=[RESOURCE_1],
 				tenant="*",
 			)
-		)
+
 
 	def test_tenant_access(self):
 		self.assertTrue(
 			has_tenant_access(
 				claims=self.Claims,
 				tenant=TENANT_1,
-			)
+			),
+			"Superuser must have unrestricted tenant access.",
 		)
-		self.assertTrue(
+
+		with self.assertRaises(ValueError):
 			has_tenant_access(
-				claims=self.Claims,
-				tenant=TENANT_2,
-			)
-		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_tenant_access(
 				claims=self.Claims,
 				tenant="*",
 			)
-		)
-		self.assertRaises(
-			ValueError,
-			lambda: has_tenant_access(
+
+		with self.assertRaises(ValueError):
+			has_tenant_access(
 				claims=self.Claims,
 				tenant=None,
 			)
-		)
+
 
 	def test_superuser(self):
 		self.assertTrue(
 			is_superuser(
 				claims=self.Claims,
-			)
+			),
+			"Superuser resource must grant superuser privileges.",
 		)
