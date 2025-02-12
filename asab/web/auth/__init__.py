458cc5c3a29b988d0ab6b3a85739c93c9ab5e8ce
Robin Hruška <byewokko@seznam.cz>
2025-02-12 16:34:52 +0100

    nicer imports

--- asab/web/auth/__init__.py
+++ asab/web/auth/__init__.py
@@ -1,10 +1,13 @@
 from .decorator import require, noauth
 from .service import AuthService
 from .authorization import Authorization
+from . import utils, providers
 
 __all__ = (
 	"AuthService",
 	"Authorization",
 	"require",
 	"noauth",
+	"utils",
+	"providers",
 )
