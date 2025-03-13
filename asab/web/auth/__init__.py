bbd291fdc104fcd97b86896dedad53999855110b
Robin Hruška <11579460+byewokko@users.noreply.github.com>
2025-03-13 16:28:27 +0100

    Remove mandatory dependency on jwcrypto (#673)
    * prevent unwanted jwcrypto dependency
    
    * prevent unwanted jwcrypto dependency

--- asab/web/auth/__init__.py
+++ asab/web/auth/__init__.py
@@ -1,7 +1,7 @@
 from .decorator import require, require_superuser, noauth
 from .service import AuthService
 from .authorization import Authorization, SUPERUSER_RESOURCE_ID
-from . import utils, providers
+
 
 __all__ = (
 	"AuthService",
@@ -9,7 +9,5 @@ __all__ = (
 	"require",
 	"require_superuser",
 	"noauth",
-	"utils",
-	"providers",
 	"SUPERUSER_RESOURCE_ID",
 )
