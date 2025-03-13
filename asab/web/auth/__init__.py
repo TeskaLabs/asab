2da04b868ae57018d6684cafd55a2823f79d0d50
Robin Hruška <byewokko@seznam.cz>
2025-03-13 15:52:06 +0100

    prevent unwanted jwcrypto dependency

--- asab/web/auth/__init__.py
+++ asab/web/auth/__init__.py
@@ -1,7 +1,7 @@
 from .decorator import require, require_superuser, noauth
 from .service import AuthService
 from .authorization import Authorization, SUPERUSER_RESOURCE_ID
-from . import utils, providers
+
 
 __all__ = (
 	"AuthService",
