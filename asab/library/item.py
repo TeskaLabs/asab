883fe3547999509a23b393bef6e321bd396472bf
Mithun Shivashankar <mithun.shivashankar@teskalabs.com>
2026-02-06 11:12:59 +0100

    Update item layer description

--- asab/library/item.py
+++ asab/library/item.py
@@ -10,14 +10,12 @@ class LibraryItem:
     Attributes:
         name (str): The absolute path of the Item. It can be directly fed into `LibraryService.read(...)`.
         type (str): Can be either `dir` if the Item is a directory or `item` if Item is of any other type.
-            layers (list[int | str]): Identifiers of layers in which this item was found.
-        Values are provider-defined and treated as opaque identifiers.
-        Examples: 0, 1, "0:global", "0:tenant", "0:personal".
+        layers (list[str]): Identifiers of layers in which this item was found.
+        Examples only as strings, e.g. "0", "1", "0:global", "0:tenant", "0:personal".
         providers (list): List of `LibraryProvider` objects containing this Item.
         disabled (bool): `True` if the Item is disabled, `False` otherwise. If the Item is disabled, `LibraryService.read(...)` will return `None`.
         favorite (bool): True if the Item is marked as a favorite.
         override (int): If `True`, this item is marked as an override for the providers with the same Item name.
-        target (str): Specifies the target context, e.g., "tenant" or "global". Defaults to "global".
     """
     name: str
     type: str
