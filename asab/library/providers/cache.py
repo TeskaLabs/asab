3617df97ea54152a77ff817bbc653ce6cc2746d0
Mithun Shivashankar <mithun.shivashankar@teskalabs.com>
2026-02-24 06:00:10 +0100

    Add debug message after read for precise hit

--- asab/library/providers/cache.py
+++ asab/library/providers/cache.py
@@ -68,15 +68,17 @@ class CacheLibraryProvider(FileSystemLibraryProvider):
 	async def read(self, path):
 		if not self._cache_live():
 			return None
-		L.warning(
-			"Cache hit",
+
+		itemio = await super().read(path)
+		L.debug(
+			"Cache read %s",
+			"hit" if itemio is not None else "miss",
 			struct_data={
 				"path": path,
-				"provider": self.__class__.__name__,
-				"base": getattr(self, "global_link", getattr(self, "cache_dir", None)),
+				"base": self.cache_dir,
 			},
 		)
-		return await super().read(path)
+		return itemio
 
 	async def list(self, path):
 		if not self._cache_live():
